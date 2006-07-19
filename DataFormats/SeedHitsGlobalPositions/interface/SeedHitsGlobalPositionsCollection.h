30f3899bdd9d3955834bcc0c59616c87a41e876e
gutsche <>
2006-07-19 17:15:12 +0000

    first commit of seeds global positions object

---
+++ DataFormats/SeedHitsGlobalPositions/interface/SeedHitsGlobalPositionsCollection.h
@@ -0,0 +1,9 @@
+#ifndef DATAFORMATS_SEEDHITSGLOBALPOSITIONSCOLLECTION_H
+#define DATAFORMATS_SEEDHITSGLOBALPOSITIONSCOLLECTION_H
+
+#include <vector>
+#include "DataFormats/SeedHitsGlobalPositions/interface/SeedHitsGlobalPositions.h"
+
+typedef std::vector<SeedHitsGlobalPositions> SeedHitsGlobalPositionsCollection;
+
+#endif // 
---
+++ DataFormats/SeedHitsGlobalPositions/src/classes.h
@@ -0,0 +1,14 @@
+#ifndef SEEDHITSGLOBALPOSITIONS_CLASSES_H
+#define SEEDHITSGLOBALPOSITIONS_CLASSES_H
+
+#include "DataFormats/SeedHitsGlobalPositions/interface/SeedHitsGlobalPositionsCollection.h"
+#include "DataFormats/Common/interface/Wrapper.h"
+#include <vector>
+
+namespace {
+  namespace {
+    edm::Wrapper<SeedHitsGlobalPositionsCollection> recHitGlobalPositionCollectionWrapper;
+  }
+}
+
+#endif // SEEDHITSGLOBALPOSITIONS_CLASSES_H
