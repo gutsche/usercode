95f6337e96e62869745ba2098be100555fdfc728
gutsche <>
2006-08-31 00:44:41 +0000

    parameter-sets and condor jdl for profiling

---
+++ Profiling/CMSSW_1_0_0_pre2/condor/cmssw_on_condor.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+#
+# variables from arguments string in jdl
+#
+# format:
+#
+# 1: condor cluster number
+# 2: condor process number
+# 3: CMSSW_DIR
+# 4: RUN_DIR
+# 5: PARAMETER_SET (full path, has to contain all needed files in PoolSource and filled following variables with keywords: FrameworkJobReport xml to CONDOR_FRAMEWORKJOBREPORT, maxEvents = CONDOR_MAXEVENTS, skipEvents = CONDOR_SKIPEVENTS, output fileName = CONDOR_OUTPUTFILENAME)
+# 6: NUM_EVENTS_PER_JOB
+#
+
+CONDOR_CLUSTER=$1
+CONDOR_PROCESS=$2
+CMSSW_DIR=$3
+RUN_DIR=$4
+PARAMETER_SET=$5
+NUM_EVENTS_PER_JOB=$6
+
+#
+# header 
+#
+
+echo ""
+echo "CMSSW on Condor"
+echo ""
+
+START_TIME=`/bin/date`
+echo "started at $START_TIME"
+
+echo ""
+echo "parameter set:"
+echo "CONDOR_CLUSTER: $CONDOR_CLUSTER"
+echo "CONDOR_PROCESS: $CONDOR_PROCESS"
+echo "CMSSW_DIR: $CMSSW_DIR"
+echo "RUN_DIR: $RUN_DIR"
+echo "PRAMETER_SET: $PARAMETER_SET"
+echo "NUM_EVENTS_PER_JOB: $NUM_EVENTS_PER_JOB"
+
+#
+# setup software environment at FNAL for the given CMSSW release
+#
+source /afs/fnal.gov/files/code/cms/setup/shrc uaf
+export SCRAM_ARCH=slc3_ia32_gcc323
+cd $CMSSW_DIR
+eval `scramv1 runtime -sh`
+
+#
+# change to output directory
+#
+cd $RUN_DIR
+
+#
+# modify parameter-set
+#
+
+FINAL_PARAMETER_SET_NAME=`echo cmssw_${CONDOR_CLUSTER}_${CONDOR_PROCESS}`
+FINAL_PARAMETER_SET=`echo $FINAL_PARAMETER_SET_NAME.cfg`
+FINAL_LOG=`echo $FINAL_PARAMETER_SET_NAME.log`
+FINAL_FILENAME=`echo $FINAL_PARAMETER_SET_NAME.root`
+FINAL_FRAMEWORKJOBREPORT=`echo $FINAL_PARAMETER_SET_NAME`
+echo ""
+echo "Writing final parameter-set: $FINAL_PARAMETER_SET to RUN_DIR: $RUN_DIR"
+echo ""
+
+let "skip = $CONDOR_PROCESS * NUM_EVENTS_PER_JOB"
+cat $PARAMETER_SET | sed -e s/CONDOR_MAXEVENTS/$NUM_EVENTS_PER_JOB/ | sed -e s/CONDOR_SKIPEVENTS/$skip/ | sed -e s/CONDOR_OUTPUTFILENAME/$FINAL_FILENAME/ | sed -e s/CONDOR_FRAMEWORKJOBREPORT/$FINAL_FRAMEWORKJOBREPORT/ > $FINAL_PARAMETER_SET
+
+#
+# run cmssw
+#
+
+echo "run: time cmsRun $FINAL_PARAMETER_SET > $FINAL_LOG 2>&1"
+time cmsRun $FINAL_PARAMETER_SET >> $FINAL_LOG 2>&1
+
+#
+# end run
+#
+
+echo ""
+START_TIME=`/bin/date`
+echo "started at $START_TIME"
