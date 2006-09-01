ac3dd7857179c59fb4819dfe7301f6af0ef6a15b
gutsche <>
2006-09-01 00:11:55 +0000

    improvements, multiple logfile handling

--- Profiling/extractTimingFromCMSSW.py
+++ Profiling/extractTimingFromCMSSW.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-import sys, os, getopt
+import sys, os, getopt, glob
 
 def main(argv) :
     """
@@ -10,7 +10,7 @@ def main(argv) :
     activate timing service in CMSSW and use this python script tp average timing per module
 
     required parameters
-    --logfile <name>                          :       logfile name
+    --logfiles <names>                        :       logfile names
 
     optional parameters                       :
     --help (-h)                               :       help
@@ -42,98 +42,84 @@ def main(argv) :
         print main.__doc__
         sys.exit(2)
         
-    try:
-        logfile = open(logfilename)
-    except IOError:
-        print ''
-        print 'Could not open logfile: ',logfilename
-        print main.__doc__
-        sys.exit(2)
-
     # instantiate dictionaries and counters
     timeEvent       = 0
     timeEventCount  = 0
     timeModuleDict  = {}
     timeModuleCount = {}
 
-    line = logfile.readline()
-    tempeventtime = 0
-    addedmoduletime = 0
-    while line :
-        line = line.strip()
-        if line.count('TimeEvent>') > 0 :
-            if debug :
-                print 'TimeEvent line:',line
-            linearray = line.split()
-            try:
-                tempeventtime = float(linearray[-1])
-                timeEvent      += tempeventtime
-                timeEventCount += 1
-            except ValueError:
-                print 'line:',line,'could not be used to extract time of the event'
-        if line.count('TimeModule>') > 0 :
-            if debug :
-                print 'TimeModule line:',line
-            try:
-                linearray = line.split()
-                if len(linearray) == 6 :
-                    temptime = float(linearray[-1])
-                    addedmoduletime += temptime
-                    tempmodule = linearray[-3]
-                    if tempmodule in timeModuleDict.keys() :
-                        timeModuleDict[tempmodule] += temptime
-                    else :
-                        timeModuleDict[tempmodule] = temptime
-                    if tempmodule in timeModuleCount.keys() :
-                        timeModuleCount[tempmodule] += 1
-                    else :
-                        timeModuleCount[tempmodule] = 1
-            except ValueError:
-                print 'line:',line,'could not be used to extract time of a module'
+
+    for filename in glob.glob(logfilename) :
+        try:
+            logfile = open(filename)
+        except IOError:
+            print ''
+            print 'Could not open logfile: ',logfilename
+            print main.__doc__
+            sys.exit(2)
+
         line = logfile.readline()
-        if tempeventtime != 0 :
-            # print '%2.5f' % (tempeventtime-addedmoduletime)
-            tempeventtime = 0
-            addedmoduletime = 0
+        tempeventtime = 0
+        addedmoduletime = 0
+        while line :
+            line = line.strip()
+            if line.count('TimeEvent>') > 0 :
+                if debug :
+                    print 'TimeEvent line:',line
+                linearray = line.split()
+                try:
+                    tempeventtime = float(linearray[-1])
+                    timeEvent      += tempeventtime
+                    timeEventCount += 1
+                except ValueError:
+                    print 'line:',line,'could not be used to extract time of the event'
+            if line.count('TimeModule>') > 0 :
+                if debug :
+                    print 'TimeModule line:',line
+                try:
+                    linearray = line.split()
+                    if len(linearray) == 6 :
+                        temptime = float(linearray[-1])
+                        addedmoduletime += temptime
+                        tempmodule = linearray[-3]
+                        if tempmodule in timeModuleDict.keys() :
+                            timeModuleDict[tempmodule] += temptime
+                        else :
+                            timeModuleDict[tempmodule] = temptime
+                        if tempmodule in timeModuleCount.keys() :
+                            timeModuleCount[tempmodule] += 1
+                        else :
+                            timeModuleCount[tempmodule] = 1
+                except ValueError:
+                    print 'line:',line,'could not be used to extract time of a module'
+            line = logfile.readline()
+            if tempeventtime != 0 :
+                tempeventtime = 0
+                addedmoduletime = 0
 
     for module in timeModuleDict.keys() :
-        print 'module: %30s time: %2.5f percentage: %02.1f' % (module, timeModuleDict[module]/timeModuleCount[module], (timeModuleDict[module]/timeModuleCount[module])/(timeEvent/timeEventCount)*100)
+        print 'module: %30s time: %7.5f percentage: %03.1f' % (module, timeModuleDict[module]/timeModuleCount[module], (timeModuleDict[module]/timeModuleCount[module])/(timeEvent/timeEventCount)*100)
 
-    print 'event :  %2.5f' % (timeEvent/timeEventCount)
+    print 'events: %30d time: %7.5f' % (timeEventCount, timeEvent/timeEventCount)
 
-    if 'PoolOutputModule' in timeModuleDict.keys():
-        print ''
-        print 'remove PoolOutput Module form statistic'
-        print ''
-        tempTimeEvent = 0
-        for module in timeModuleDict.keys() :
-            if module != 'PoolOutputModule' :
-                tempTimeEvent += timeModuleDict[module]/timeModuleCount[module]
-        for module in timeModuleDict.keys() :
-            if module != 'PoolOutputModule' :
-                print 'module: %30s time: %2.5f percentage: %02.1f' % (module, timeModuleDict[module]/timeModuleCount[module], (timeModuleDict[module]/timeModuleCount[module])/(tempTimeEvent)*100)
+    # calculate time per event for RS and Ckf
 
-    modules = ['RoadSearchCloudCleaner','TrackProducer','RoadSearchTrackCandidateMaker','RoadSearchSeedFinder','RoadSearchCloudMaker']
+    rsmodules = ['roadSearchSeeds','rawRoadSearchClouds','cleanRoadSearchClouds','rsTrackCandidates','rsWithMaterialTracks']
+    ckfmodules = ['globalMixedSeeds','ckfTrackCandidates','ctfWithMaterialTracks']
 
-    doTracking = 1
-    for target in modules:
-        if target not in timeModuleDict.keys():
-            doTracking = 0
+    rsTimePerEvent = 0.
+    for rsmodule in rsmodules :
+        rsTimePerEvent += timeModuleDict[rsmodule]/timeModuleCount[rsmodule]
 
-    if doTracking :
-        print ''
-        print 'Statistics only for modules:'
-        print ''
-        for module in modules:
-            print module
-        print ''
-        tempTimeEvent = 0
-        for module in modules :
-            if module != 'PoolOutputModule' :
-                tempTimeEvent += timeModuleDict[module]/timeModuleCount[module]
-        for module in modules :
-            if module != 'PoolOutputModule' :
-                print 'module: %30s time: %2.5f percentage: %02.1f' % (module, timeModuleDict[module]/timeModuleCount[module], (timeModuleDict[module]/timeModuleCount[module])/(tempTimeEvent)*100)
+    ckfTimePerEvent = 0.
+    for ckfmodule in ckfmodules :
+        ckfTimePerEvent += timeModuleDict[ckfmodule]/timeModuleCount[ckfmodule]
+
+    print ''
+    print 'module: %30s time: %7.5f percentage: %03.1f' % ('RS', rsTimePerEvent, rsTimePerEvent/(timeEvent/timeEventCount)*100)
+    print 'module: %30s time: %7.5f percentage: %03.1f' % ('CKF', ckfTimePerEvent, ckfTimePerEvent/(timeEvent/timeEventCount)*100)
+
+
         
 
 if __name__ == '__main__' :
