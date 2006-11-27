8ca6652d53dafe811fcf63e6d410c7b13d20cf9a
gutsche <>
2006-11-27 23:46:55 +0000

    rename to make module name unique

--- GutSoftAnalyzers/TrajectorySeedAnalyzer/src/SealModules.cc
+++
@@ -1,6 +0,0 @@
-#include "PluginManager/ModuleDef.h"
-#include "FWCore/Framework/interface/MakerMacros.h"
-#include "GutSoftAnalyzers/TrajectorySeedAnalyzer/interface/TrajectorySeedAnalyzer.h"
-
-DEFINE_SEAL_MODULE();
-DEFINE_ANOTHER_FWK_MODULE(TrajectorySeedAnalyzer)
