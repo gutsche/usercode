34d5f12185209c40eb96259e7153e737e5ea09e0
dmytro <>
2007-02-16 11:53:33 +0000

    Modified Muons class to provide a skeleton of muon selectors

--- CMS1/Muons/interface/Muons.h
+++ CMS1/Muons/interface/Muons.h
@@ -10,25 +10,56 @@
 // Original Author: Oliver Gutsche, gutsche@fnal.gov
 // Created:         Mon Jan 29 16:40:39 UTC 2007
 //
-// $Author: gutsche $
-// $Date: 2007/01/22 01:35:05 $
-// $Revision: 1.2 $
+// $Author$
+// $Date:$
+// $Revision:$
 //
 
 #include "DataFormats/MuonReco/interface/Muon.h"
+#include "DataFormats/MuonReco/interface/MuIsoDeposit.h"
+#include "DataFormats/MuonReco/interface/MuonWithMatchInfo.h"
+#include "DataFormats/TrackReco/interface/Track.h"
+#include "CMS1/Base/interface/Cuts.h"
 
 namespace cms1 {
+   class Muons  {
+    public:
+      struct MuonData
+	{
+	   MuonData(): globalMuonCollection(0), muonIdCollection(0),
+	   muonIsolationCollection(0), trackCollection(0) 
+	     {}
 
-  class Muons  {
-
-  public:
-    Muons();
-    ~Muons();
-   
-    bool GlobalMuons(const reco::MuonCollection *muonCollection, 
-		     unsigned int number,
-		     double pt);
- 
+	   const std::vector<reco::Muon>*              globalMuonCollection;
+	   const std::vector<reco::MuonWithMatchInfo>* muonIdCollection;
+	   const std::vector<reco::MuIsoDeposit>*      muonIsolationCollection;
+	   const std::vector<reco::Track>*             trackCollection;
+	};
+     
+      // These are "types of muons" that we define.  We can add as amany as we want as
+      // people invent new muon requirements
+      enum MuonType { AllTracks, AllGlobalMuons, TightGlobalMuons, AllMuonWithDetectorInfos };
+     
+      // We need to have pointers from the event to the collections that we might want to use.
+      // This is done by setting the data_ private member at the beginning of the analysis of 
+      // each event.  
+      void setData( MuonData& data ) { data_ = data; }  // sets data_
+      const MuonData& getData() const { return data_; } // gets data_ back
+      MuonData& getData() { return data_; }            // gaets data_ back
+     
+      // This is the function that does all the work
+      std::vector<const reco::Muon*> getMuons (const MuonType, 
+					       const Cuts&,
+					       bool isolated = true);
+      
+      // a trivial function that uses getMuons to return number of 
+      // muons of a given type in the event
+      int numberOfMuons(const MuonType type, const Cuts& cuts) {
+	 return getMuons(type,cuts).size();
+      }
+      
+    private:
+      MuonData data_;
   };
 }
 
--- CMS1/Muons/src/Muons.cc
+++ CMS1/Muons/src/Muons.cc
@@ -8,40 +8,48 @@
 // Created:         Mon Jan 29 16:40:39 UTC 2007
 //
 // $Author: gutsche $
-// $Date: 2007/01/22 01:35:05 $
-// $Revision: 1.2 $
+// $Date: 2007/01/29 18:15:28 $
+// $Revision: 1.1 $
 //
 
 #include "CMS1/Muons/interface/Muons.h"
 
-cms1::Muons::Muons()
+std::vector<const reco::Muon*> cms1::Muons::getMuons(const MuonType muonType,
+						     const Cuts& cuts,
+						     const bool isolated )
 {
-}
+   // this is the output list
+   std::vector<const reco::Muon*> output_list;
+   
+   // Only TightGlobalMuons are implemented for now
+   switch (muonType) {
+    case TightGlobalMuons:
+	{
+	   if (! data_.globalMuonCollection) {
+	      std::cout << "ERROR: global muon collection is not set" << std::endl;
+	      return output_list;
+	   }
+	   for ( std::vector<reco::Muon>::const_iterator muon = data_.globalMuonCollection->begin();
+		 muon != data_.globalMuonCollection->end();
+		 ++muon ) 
+	     {
+		// Here we make simple cuts in a standard way
+		if ( cuts.testTrack(*muon->track().get()) ) output_list.push_back(&*muon);
+	     }
+      
+	   // At this point the output_list has been filled with muons passing the simple cuts
+	   // In general, we will want to add more cuts.  This could go here.  It can be
+	   // done several ways, for example:
+	   //     Make another empty vector "final_output_list"
+	   //     loop over the vector "output_list" that we just made and add to final_output_list
+	   //     those muons that pass more complicated cuts
+	   // For now, none of this is implemented    
 
-
-cms1::Muons::~Muons()
-{
-}
-
-bool cms1::Muons::GlobalMuons(const reco::MuonCollection *muonCollection, 
-			      unsigned int number,
-			      double pt) {
-
-  // return value
-  bool result = false;
-
-  unsigned int counter = 0;
-  for ( reco::MuonCollection::const_iterator muon = muonCollection->begin();
-	muon != muonCollection->end();
-	++muon ) {
-    if (muon->pt() > pt ) {
-      ++counter;
-    }
-  }
-
-  if ( counter >= number ) {
-    result = true;
-  }
-    
-  return result;
+	}
+      break;
+      // You get here if you have requested a "muonType" that is not implemented
+    default:
+      std::cout << "Unkown or not implemented type" << std::endl;
+   }
+   return output_list;
 }
