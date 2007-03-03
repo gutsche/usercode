172d6b89a3bb4531a56e35acd209abf3dfc9a9ce
gutsche <>
2007-03-03 16:27:36 +0000

    MC generation for 1_3_0_pre5

---
+++ GutSoftConfigurations/Scripts/do_simulation.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+./createSimulationProject --project_name 070303_01 --parameter_set simulation_gun.cfg --generator_cff single_mu_pt_1_minus.cff --seed 123 --events 1000 --jobs 5
+./createSimulationProject --project_name 070303_02 --parameter_set simulation_gun.cfg --generator_cff single_mu_pt_2_minus.cff --seed 123 --events 1000 --jobs 5
+./createSimulationProject --project_name 070303_03 --parameter_set simulation_gun.cfg --generator_cff single_mu_pt_5_minus.cff --seed 123 --events 1000 --jobs 5
+./createSimulationProject --project_name 070303_04 --parameter_set simulation_gun.cfg --generator_cff single_mu_pt_10_minus.cff --seed 123 --events 1000 --jobs 5
+./createSimulationProject --project_name 070303_05 --parameter_set simulation_gun.cfg --generator_cff single_mu_pt_25_minus.cff --seed 123 --events 1000 --jobs 5
+./createSimulationProject --project_name 070303_06 --parameter_set simulation_gun.cfg --generator_cff single_mu_pt_100_minus.cff --seed 123 --events 1000 --jobs 5
+
+./createSimulationProject --project_name 070303_11 --parameter_set simulation_gun.cfg --generator_cff single_mu_pt_1_plus.cff --seed 123 --events 1000 --jobs 5
+./createSimulationProject --project_name 070303_12 --parameter_set simulation_gun.cfg --generator_cff single_mu_pt_2_plus.cff --seed 123 --events 1000 --jobs 5
+./createSimulationProject --project_name 070303_13 --parameter_set simulation_gun.cfg --generator_cff single_mu_pt_5_plus.cff --seed 123 --events 1000 --jobs 5
+./createSimulationProject --project_name 070303_14 --parameter_set simulation_gun.cfg --generator_cff single_mu_pt_10_plus.cff --seed 123 --events 1000 --jobs 5
+./createSimulationProject --project_name 070303_15 --parameter_set simulation_gun.cfg --generator_cff single_mu_pt_25_plus.cff --seed 123 --events 1000 --jobs 5
+./createSimulationProject --project_name 070303_16 --parameter_set simulation_gun.cfg --generator_cff single_mu_pt_100_plus.cff --seed 123 --events 1000 --jobs 5
+
+./createSimulationProject --project_name 070303_21 --parameter_set simulation_gun.cfg --generator_cff single_e_pt_1_minus.cff --seed 123 --events 1000 --jobs 5
+./createSimulationProject --project_name 070303_22 --parameter_set simulation_gun.cfg --generator_cff single_e_pt_2_minus.cff --seed 123 --events 1000 --jobs 5
+./createSimulationProject --project_name 070303_23 --parameter_set simulation_gun.cfg --generator_cff single_e_pt_5_minus.cff --seed 123 --events 1000 --jobs 5
+./createSimulationProject --project_name 070303_24 --parameter_set simulation_gun.cfg --generator_cff single_e_pt_10_minus.cff --seed 123 --events 1000 --jobs 5
+./createSimulationProject --project_name 070303_25 --parameter_set simulation_gun.cfg --generator_cff single_e_pt_25_minus.cff --seed 123 --events 1000 --jobs 5
+./createSimulationProject --project_name 070303_26 --parameter_set simulation_gun.cfg --generator_cff single_e_pt_100_minus.cff --seed 123 --events 1000 --jobs 5
+
+./createSimulationProject --project_name 070303_31 --parameter_set simulation_gun.cfg --generator_cff single_e_pt_1_plus.cff --seed 123 --events 1000 --jobs 5
+./createSimulationProject --project_name 070303_32 --parameter_set simulation_gun.cfg --generator_cff single_e_pt_2_plus.cff --seed 123 --events 1000 --jobs 5
+./createSimulationProject --project_name 070303_33 --parameter_set simulation_gun.cfg --generator_cff single_e_pt_5_plus.cff --seed 123 --events 1000 --jobs 5
+./createSimulationProject --project_name 070303_34 --parameter_set simulation_gun.cfg --generator_cff single_e_pt_10_plus.cff --seed 123 --events 1000 --jobs 5
+./createSimulationProject --project_name 070303_35 --parameter_set simulation_gun.cfg --generator_cff single_e_pt_25_plus.cff --seed 123 --events 1000 --jobs 5
+./createSimulationProject --project_name 070303_36 --parameter_set simulation_gun.cfg --generator_cff single_e_pt_100_plus.cff --seed 123 --events 1000 --jobs 5
+
+./createSimulationProject --project_name 070303_41 --parameter_set simulation_gun.cfg --generator_cff single_pi_pt_1_minus.cff --seed 123 --events 1000 --jobs 5
+./createSimulationProject --project_name 070303_42 --parameter_set simulation_gun.cfg --generator_cff single_pi_pt_2_minus.cff --seed 123 --events 1000 --jobs 5
+./createSimulationProject --project_name 070303_43 --parameter_set simulation_gun.cfg --generator_cff single_pi_pt_5_minus.cff --seed 123 --events 1000 --jobs 5
+./createSimulationProject --project_name 070303_44 --parameter_set simulation_gun.cfg --generator_cff single_pi_pt_10_minus.cff --seed 123 --events 1000 --jobs 5
+./createSimulationProject --project_name 070303_45 --parameter_set simulation_gun.cfg --generator_cff single_pi_pt_25_minus.cff --seed 123 --events 1000 --jobs 5
+./createSimulationProject --project_name 070303_46 --parameter_set simulation_gun.cfg --generator_cff single_pi_pt_100_minus.cff --seed 123 --events 1000 --jobs 5
+
+./createSimulationProject --project_name 070303_51 --parameter_set simulation_gun.cfg --generator_cff single_pi_pt_1_plus.cff --seed 123 --events 1000 --jobs 5
+./createSimulationProject --project_name 070303_52 --parameter_set simulation_gun.cfg --generator_cff single_pi_pt_2_plus.cff --seed 123 --events 1000 --jobs 5
+./createSimulationProject --project_name 070303_53 --parameter_set simulation_gun.cfg --generator_cff single_pi_pt_5_plus.cff --seed 123 --events 1000 --jobs 5
+./createSimulationProject --project_name 070303_54 --parameter_set simulation_gun.cfg --generator_cff single_pi_pt_10_plus.cff --seed 123 --events 1000 --jobs 5
+./createSimulationProject --project_name 070303_55 --parameter_set simulation_gun.cfg --generator_cff single_pi_pt_25_plus.cff --seed 123 --events 1000 --jobs 5
+./createSimulationProject --project_name 070303_56 --parameter_set simulation_gun.cfg --generator_cff single_pi_pt_100_plus.cff --seed 123 --events 1000 --jobs 5
+
+./createSimulationProject --project_name 070303_61 --parameter_set simulation_pythia.cfg --generator_cff minbias.cff --seed 123 --events 100 --jobs 30
+
+./createSimulationProject --project_name 070303_71 --parameter_set simulation_pythia_lowlumipileup.cfg --generator_cff bjets_120_170.cff --seed 123 --events 100 --jobs 30
+./createSimulationProject --project_name 070303_72 --parameter_set simulation_pythia_lowlumipileup.cfg --generator_cff ttbar.cff --seed 123 --events 100 --jobs 30
