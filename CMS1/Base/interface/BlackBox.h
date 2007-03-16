fec0adc3fa66c4aaff8cd0cfd39bd4494effc6f4
dmytro <>
2007-03-16 07:00:21 +0000

    Base class for black boxes to provide interface to the EventData

---
+++ CMS1/Base/interface/BlackBox.h
@@ -0,0 +1,36 @@
+#ifndef CMS1BlackBox_h
+#define CMS1BlackBox_h
+
+//
+// Package:         CMS1/Base
+// Class:           BlackBox
+// 
+// Description:     common functions and data members for various Black Boxes
+//
+// Original Author: Dmytro Kovalskyi
+//
+// $Author: dmytro $
+// $Date: 2007/03/09 19:40:49 $
+// $Revision: 1.1 $
+//
+
+#include "CMS1/Base/interface/EventData.h"
+
+namespace cms1 {
+   class BlackBox  {
+    public:
+      BlackBox():data_(0) {}
+      
+      // We need to have a pointer to the event data that we might want to use.
+      // This is done by setting the data_ private member at the beginning of 
+      // the analysis for each event.
+      // The black box is allowed to add new information to the EventData.
+      void setEventData( EventData* data ) { data_ = data; } 
+      EventData* getEventData() { return data_; } 
+
+    protected:
+      EventData* data_;
+  };
+}
+
+#endif
