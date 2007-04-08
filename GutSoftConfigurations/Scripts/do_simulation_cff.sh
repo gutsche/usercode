7c41a9c64f0cc8daf35cf24b53c3957675d6d1ea
gutsche <>
2007-04-08 19:59:00 +0000

    changes for 1_4_0_pre2

--- GutSoftConfigurations/Scripts/do_simulation_cff.sh
+++ GutSoftConfigurations/Scripts/do_simulation_cff.sh
@@ -2,188 +2,188 @@
 
 # single mu minus
 
-cd 070402_02_01
-$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_3_0_single_mu_pt_1_minus.cff
+cd 070402_03_01
+$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_4_0_pre2_single_mu_pt_1_minus.cff
 cd ..
 
-cd 070402_02_02
-$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_3_0_single_mu_pt_2_minus.cff
+cd 070402_03_02
+$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_4_0_pre2_single_mu_pt_2_minus.cff
 cd ..
 
-cd 070402_02_03
-$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_3_0_single_mu_pt_5_minus.cff
+cd 070402_03_03
+$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_4_0_pre2_single_mu_pt_5_minus.cff
 cd ..
 
-cd 070402_02_04
-$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_3_0_single_mu_pt_10_minus.cff
+cd 070402_03_04
+$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_4_0_pre2_single_mu_pt_10_minus.cff
 cd ..
 
-cd 070402_02_05
-$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_3_0_single_mu_pt_25_minus.cff
+cd 070402_03_05
+$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_4_0_pre2_single_mu_pt_25_minus.cff
 cd ..
 
-cd 070402_02_06
-$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_3_0_single_mu_pt_100_minus.cff
+cd 070402_03_06
+$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_4_0_pre2_single_mu_pt_100_minus.cff
 cd ..
 
 # single mu plus
 
-cd 070402_02_11
-$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_3_0_single_mu_pt_1_plus.cff
+cd 070402_03_11
+$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_4_0_pre2_single_mu_pt_1_plus.cff
 cd ..
 
-cd 070402_02_12
-$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_3_0_single_mu_pt_2_plus.cff
+cd 070402_03_12
+$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_4_0_pre2_single_mu_pt_2_plus.cff
 cd ..
 
-cd 070402_02_13
-$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_3_0_single_mu_pt_5_plus.cff
+cd 070402_03_13
+$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_4_0_pre2_single_mu_pt_5_plus.cff
 cd ..
 
-cd 070402_02_14
-$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_3_0_single_mu_pt_10_plus.cff
+cd 070402_03_14
+$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_4_0_pre2_single_mu_pt_10_plus.cff
 cd ..
 
-cd 070402_02_15
-$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_3_0_single_mu_pt_25_plus.cff
+cd 070402_03_15
+$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_4_0_pre2_single_mu_pt_25_plus.cff
 cd ..
 
-cd 070402_02_16
-$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_3_0_single_mu_pt_100_plus.cff
+cd 070402_03_16
+$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_4_0_pre2_single_mu_pt_100_plus.cff
 cd ..
 
 # single e minus
 
-cd 070402_02_21
-$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_3_0_single_e_pt_1_minus.cff
+cd 070402_03_21
+$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_4_0_pre2_single_e_pt_1_minus.cff
 cd ..
 
-cd 070402_02_22
-$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_3_0_single_e_pt_2_minus.cff
+cd 070402_03_22
+$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_4_0_pre2_single_e_pt_2_minus.cff
 cd ..
 
-cd 070402_02_23
-$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_3_0_single_e_pt_5_minus.cff
+cd 070402_03_23
+$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_4_0_pre2_single_e_pt_5_minus.cff
 cd ..
 
-cd 070402_02_24
-$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_3_0_single_e_pt_10_minus.cff
+cd 070402_03_24
+$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_4_0_pre2_single_e_pt_10_minus.cff
 cd ..
 
-cd 070402_02_25
-$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_3_0_single_e_pt_25_minus.cff
+cd 070402_03_25
+$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_4_0_pre2_single_e_pt_25_minus.cff
 cd ..
 
-cd 070402_02_26
-$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_3_0_single_e_pt_100_minus.cff
+cd 070402_03_26
+$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_4_0_pre2_single_e_pt_100_minus.cff
 cd ..
 
 # single e plus
 
-cd 070402_02_31
-$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_3_0_single_e_pt_1_plus.cff
+cd 070402_03_31
+$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_4_0_pre2_single_e_pt_1_plus.cff
 cd ..
 
-cd 070402_02_32
-$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_3_0_single_e_pt_2_plus.cff
+cd 070402_03_32
+$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_4_0_pre2_single_e_pt_2_plus.cff
 cd ..
 
-cd 070402_02_33
-$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_3_0_single_e_pt_5_plus.cff
+cd 070402_03_33
+$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_4_0_pre2_single_e_pt_5_plus.cff
 cd ..
 
-cd 070402_02_34
-$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_3_0_single_e_pt_10_plus.cff
+cd 070402_03_34
+$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_4_0_pre2_single_e_pt_10_plus.cff
 cd ..
 
-cd 070402_02_35
-$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_3_0_single_e_pt_25_plus.cff
+cd 070402_03_35
+$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_4_0_pre2_single_e_pt_25_plus.cff
 cd ..
 
-cd 070402_02_36
-$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_3_0_single_e_pt_100_plus.cff
+cd 070402_03_36
+$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_4_0_pre2_single_e_pt_100_plus.cff
 cd ..
 
 # single pi minus
 
-cd 070402_02_41
-$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_3_0_single_pi_pt_1_minus.cff
+cd 070402_03_41
+$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_4_0_pre2_single_pi_pt_1_minus.cff
 cd ..
 
-cd 070402_02_42
-$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_3_0_single_pi_pt_2_minus.cff
+cd 070402_03_42
+$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_4_0_pre2_single_pi_pt_2_minus.cff
 cd ..
 
-cd 070402_02_43
-$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_3_0_single_pi_pt_5_minus.cff
+cd 070402_03_43
+$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_4_0_pre2_single_pi_pt_5_minus.cff
 cd ..
 
-cd 070402_02_44
-$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_3_0_single_pi_pt_10_minus.cff
+cd 070402_03_44
+$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_4_0_pre2_single_pi_pt_10_minus.cff
 cd ..
 
-cd 070402_02_45
-$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_3_0_single_pi_pt_25_minus.cff
+cd 070402_03_45
+$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_4_0_pre2_single_pi_pt_25_minus.cff
 cd ..
 
-cd 070402_02_46
-$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_3_0_single_pi_pt_100_minus.cff
+cd 070402_03_46
+$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_4_0_pre2_single_pi_pt_100_minus.cff
 cd ..
 
 # single pi plus
 
-cd 070402_02_51
-$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_3_0_single_pi_pt_1_plus.cff
+cd 070402_03_51
+$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_4_0_pre2_single_pi_pt_1_plus.cff
 cd ..
 
-cd 070402_02_52
-$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_3_0_single_pi_pt_2_plus.cff
+cd 070402_03_52
+$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_4_0_pre2_single_pi_pt_2_plus.cff
 cd ..
 
-cd 070402_02_53
-$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_3_0_single_pi_pt_5_plus.cff
+cd 070402_03_53
+$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_4_0_pre2_single_pi_pt_5_plus.cff
 cd ..
 
-cd 070402_02_54
-$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_3_0_single_pi_pt_10_plus.cff
+cd 070402_03_54
+$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_4_0_pre2_single_pi_pt_10_plus.cff
 cd ..
 
-cd 070402_02_55
-$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_3_0_single_pi_pt_25_plus.cff
+cd 070402_03_55
+$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_4_0_pre2_single_pi_pt_25_plus.cff
 cd ..
 
-cd 070402_02_56
-$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_3_0_single_pi_pt_100_plus.cff
+cd 070402_03_56
+$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_4_0_pre2_single_pi_pt_100_plus.cff
 cd ..
 
 # minbias
 
-cd 070402_02_61
-$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_3_0_minbias.cff
+cd 070402_03_61
+$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_4_0_pre2_minbias.cff
 cd ..
 
 # bjets
 
-cd 070402_02_71
-$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_3_0_bjets_120_170_lowlumipileup.cff
+cd 070402_03_71
+$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_4_0_pre2_bjets_120_170_lowlumipileup.cff
 cd ..
 
-cd 070402_02_72
-$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_3_0_ttbar_lowlumipileup.cff
+cd 070402_03_72
+$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_4_0_pre2_ttbar_lowlumipileup.cff
 cd ..
 
-cd 070402_02_81
-$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_3_0_single_gamma_pt_5.cff
+cd 070402_03_81
+$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_4_0_pre2_single_gamma_pt_5.cff
 cd ..
 
-cd 070402_02_82
-$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_3_0_single_gamma_pt_10.cff
+cd 070402_03_82
+$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_4_0_pre2_single_gamma_pt_10.cff
 cd ..
 
-cd 070402_02_83
-$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_3_0_single_gamma_pt_55.cff
+cd 070402_03_83
+$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_4_0_pre2_single_gamma_pt_55.cff
 cd ..
 
-cd 070402_02_84
-$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_3_0_single_gamma_pt_100.cff
+cd 070402_03_84
+$CMSSW_BASE/src/GutSoftConfigurations/Scripts/generatePoolSourceFromFiles --file "*.root" --absolute > $CMSSW_BASE/src/GutSoftConfigurations/DataSets/cmssw_1_4_0_pre2_single_gamma_pt_100.cff
 cd ..
