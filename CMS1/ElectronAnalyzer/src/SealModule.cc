3bb8257ca5033a9efd202e5cfb7eb7e726f3d0c0
sani <>
2007-04-23 09:12:48 +0000

    First implementation

---
+++ CMS1/ElectronAnalyzer/src/SealModule.cc
@@ -0,0 +1,7 @@
+#include "PluginManager/ModuleDef.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "CMS1/ElectronAnalyzer/interface/ElectronAnalyzer.h"
+//using ElectronAnalyzer;
+
+DEFINE_SEAL_MODULE();
+DEFINE_ANOTHER_FWK_MODULE(ElectronAnalyzer);
