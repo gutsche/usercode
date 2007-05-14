eea464018808362a16a02425947f5cde4388cdce
sani <>
2007-05-14 15:14:21 +0000

    MCInfo test

--- CMS1/MCInfo/plugins/SealModules.cc
+++ CMS1/MCInfo/plugins/SealModules.cc
@@ -1,9 +1,8 @@
 #include "PluginManager/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
-#include "testMCInfo.h"
-using cms1::testMCInfo;
-
+#include "testMCInfoFW.h"
+using cms1::testMCInfoFW;
 
 DEFINE_SEAL_MODULE();
-DEFINE_ANOTHER_FWK_MODULE(testMCInfo);
+DEFINE_ANOTHER_FWK_MODULE(testMCInfoFW);
 
