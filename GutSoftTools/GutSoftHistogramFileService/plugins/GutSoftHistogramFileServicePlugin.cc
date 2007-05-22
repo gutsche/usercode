aa750a8702add54b0acda393259bcde41e43b25c
gutsche <>
2007-05-22 20:55:05 +0000

    split service plugin

--- GutSoftTools/GutSoftHistogramFileService/plugins/GutSoftHistogramFileServicePlugin.cc
+++ GutSoftTools/GutSoftHistogramFileService/plugins/GutSoftHistogramFileServicePlugin.cc
@@ -15,25 +15,7 @@
 
 #include "GutSoftTools/GutSoftHistogramFileService/interface/GutSoftHistogramFileService.h"
 
-GutSoftHistogramFileService::GutSoftHistogramFileService(const edm::ParameterSet& iConfig,
-							 const edm::ActivityRegistry& iActvity)
-{
+#include "FWCore/ServiceRegistry/interface/ServiceMaker.h"
 
-  // get OutputFileName parameter
-  outputFileName_ = iConfig.getUntrackedParameter<std::string>("OutputFileName");
-
-  // create GutSoftHistogramFactory
-  factory_ = new GutSoftHistogramFactory(outputFileName_);
-
-}
-
-
-GutSoftHistogramFileService::~GutSoftHistogramFileService()
-{
- 
-  if ( factory_ ) {
-    delete factory_;
-    factory_ = 0;
-  }
-
-}
+//define this as a plug-in
+DEFINE_FWK_SERVICE(GutSoftHistogramFileService);
