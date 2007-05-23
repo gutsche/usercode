127e1a47b2b8292846346646d72a2fe9c6f4a3fc
dmytro <>
2007-05-23 02:20:19 +0000

    Removed analyzer and illegal dependencies to avoid circular dependence with other packages

--- CMS1/MCInfo/plugins/SealModules.cc
+++
@@ -1,8 +0,0 @@
-#include "PluginManager/ModuleDef.h"
-#include "FWCore/Framework/interface/MakerMacros.h"
-#include "testMCInfoFW.h"
-using cms1::testMCInfoFW;
-
-DEFINE_SEAL_MODULE();
-DEFINE_ANOTHER_FWK_MODULE(testMCInfoFW);
-
