1e196112ccb806ba35a4b853a3b75b1bd48c3234
dmytro <>
2007-06-17 06:43:42 +0000

    fixed BuildFile, renamed and relocated the event display

---
+++ CMS1/EventDisplay/src/SealModules.cc
@@ -0,0 +1,6 @@
+#include "PluginManager/ModuleDef.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "CMS1/EventDisplay/interface/EventDisplay.h"
+
+DEFINE_SEAL_MODULE();
+DEFINE_ANOTHER_FWK_MODULE(EventDisplay);
