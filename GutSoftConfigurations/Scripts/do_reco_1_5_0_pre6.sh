2d1543b3d901e34e925a7d2894b0751c93f1b242
gutsche <>
2007-06-19 14:02:01 +0000

    pre6 reco

---
+++ GutSoftConfigurations/Scripts/do_reco_1_5_0_pre6.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+./createGReconstructionProject --project_name 070617_01_01 --parameter_set reconstruction.cfg --events 250 --jobs 20 --dataset_cff cmssw_1_5_0_pre6_single_mu_pt_1_minus.cff
+./createGReconstructionProject --project_name 070617_01_02 --parameter_set reconstruction.cfg --events 250 --jobs 20 --dataset_cff cmssw_1_5_0_pre6_single_mu_pt_2_minus.cff
+./createGReconstructionProject --project_name 070617_01_03 --parameter_set reconstruction.cfg --events 250 --jobs 20 --dataset_cff cmssw_1_5_0_pre6_single_mu_pt_5_minus.cff
+./createGReconstructionProject --project_name 070617_01_04 --parameter_set reconstruction.cfg --events 250 --jobs 20 --dataset_cff cmssw_1_5_0_pre6_single_mu_pt_10_minus.cff
+./createGReconstructionProject --project_name 070617_01_05 --parameter_set reconstruction.cfg --events 250 --jobs 20 --dataset_cff cmssw_1_5_0_pre6_single_mu_pt_25_minus.cff
+./createGReconstructionProject --project_name 070617_01_06 --parameter_set reconstruction.cfg --events 250 --jobs 20 --dataset_cff cmssw_1_5_0_pre6_single_mu_pt_100_minus.cff
+
+./createGReconstructionProject --project_name 070617_01_11 --parameter_set reconstruction.cfg --events 250 --jobs 20 --dataset_cff cmssw_1_5_0_pre6_single_mu_pt_1_plus.cff
+./createGReconstructionProject --project_name 070617_01_12 --parameter_set reconstruction.cfg --events 250 --jobs 20 --dataset_cff cmssw_1_5_0_pre6_single_mu_pt_2_plus.cff
+./createGReconstructionProject --project_name 070617_01_13 --parameter_set reconstruction.cfg --events 250 --jobs 20 --dataset_cff cmssw_1_5_0_pre6_single_mu_pt_5_plus.cff
+./createGReconstructionProject --project_name 070617_01_14 --parameter_set reconstruction.cfg --events 250 --jobs 20 --dataset_cff cmssw_1_5_0_pre6_single_mu_pt_10_plus.cff
+./createGReconstructionProject --project_name 070617_01_15 --parameter_set reconstruction.cfg --events 250 --jobs 20 --dataset_cff cmssw_1_5_0_pre6_single_mu_pt_25_plus.cff
+./createGReconstructionProject --project_name 070617_01_16 --parameter_set reconstruction.cfg --events 250 --jobs 20 --dataset_cff cmssw_1_5_0_pre6_single_mu_pt_100_plus.cff
+
+./createGReconstructionProject --project_name 070617_01_21 --parameter_set reconstruction.cfg --events 250 --jobs 20 --dataset_cff cmssw_1_5_0_pre6_single_e_pt_1_minus.cff
+./createGReconstructionProject --project_name 070617_01_22 --parameter_set reconstruction.cfg --events 250 --jobs 20 --dataset_cff cmssw_1_5_0_pre6_single_e_pt_2_minus.cff
+./createGReconstructionProject --project_name 070617_01_23 --parameter_set reconstruction.cfg --events 250 --jobs 20 --dataset_cff cmssw_1_5_0_pre6_single_e_pt_5_minus.cff
+./createGReconstructionProject --project_name 070617_01_24 --parameter_set reconstruction.cfg --events 250 --jobs 20 --dataset_cff cmssw_1_5_0_pre6_single_e_pt_10_minus.cff
+./createGReconstructionProject --project_name 070617_01_25 --parameter_set reconstruction.cfg --events 250 --jobs 20 --dataset_cff cmssw_1_5_0_pre6_single_e_pt_25_minus.cff
+./createGReconstructionProject --project_name 070617_01_26 --parameter_set reconstruction.cfg --events 250 --jobs 20 --dataset_cff cmssw_1_5_0_pre6_single_e_pt_100_minus.cff
+
+./createGReconstructionProject --project_name 070617_01_31 --parameter_set reconstruction.cfg --events 250 --jobs 20 --dataset_cff cmssw_1_5_0_pre6_single_e_pt_1_plus.cff
+./createGReconstructionProject --project_name 070617_01_32 --parameter_set reconstruction.cfg --events 250 --jobs 20 --dataset_cff cmssw_1_5_0_pre6_single_e_pt_2_plus.cff
+./createGReconstructionProject --project_name 070617_01_33 --parameter_set reconstruction.cfg --events 250 --jobs 20 --dataset_cff cmssw_1_5_0_pre6_single_e_pt_5_plus.cff
+./createGReconstructionProject --project_name 070617_01_34 --parameter_set reconstruction.cfg --events 250 --jobs 20 --dataset_cff cmssw_1_5_0_pre6_single_e_pt_10_plus.cff
+./createGReconstructionProject --project_name 070617_01_35 --parameter_set reconstruction.cfg --events 250 --jobs 20 --dataset_cff cmssw_1_5_0_pre6_single_e_pt_25_plus.cff
+./createGReconstructionProject --project_name 070617_01_36 --parameter_set reconstruction.cfg --events 250 --jobs 20 --dataset_cff cmssw_1_5_0_pre6_single_e_pt_100_plus.cff
+
+./createGReconstructionProject --project_name 070617_01_41 --parameter_set reconstruction.cfg --events 250 --jobs 20 --dataset_cff cmssw_1_5_0_pre6_single_pi_pt_1_minus.cff
+./createGReconstructionProject --project_name 070617_01_42 --parameter_set reconstruction.cfg --events 250 --jobs 20 --dataset_cff cmssw_1_5_0_pre6_single_pi_pt_2_minus.cff
+./createGReconstructionProject --project_name 070617_01_43 --parameter_set reconstruction.cfg --events 250 --jobs 20 --dataset_cff cmssw_1_5_0_pre6_single_pi_pt_5_minus.cff
+./createGReconstructionProject --project_name 070617_01_44 --parameter_set reconstruction.cfg --events 250 --jobs 20 --dataset_cff cmssw_1_5_0_pre6_single_pi_pt_10_minus.cff
+./createGReconstructionProject --project_name 070617_01_45 --parameter_set reconstruction.cfg --events 250 --jobs 20 --dataset_cff cmssw_1_5_0_pre6_single_pi_pt_25_minus.cff
+./createGReconstructionProject --project_name 070617_01_46 --parameter_set reconstruction.cfg --events 250 --jobs 20 --dataset_cff cmssw_1_5_0_pre6_single_pi_pt_100_minus.cff
+
+./createGReconstructionProject --project_name 070617_01_51 --parameter_set reconstruction.cfg --events 250 --jobs 20 --dataset_cff cmssw_1_5_0_pre6_single_pi_pt_1_plus.cff
+./createGReconstructionProject --project_name 070617_01_52 --parameter_set reconstruction.cfg --events 250 --jobs 20 --dataset_cff cmssw_1_5_0_pre6_single_pi_pt_2_plus.cff
+./createGReconstructionProject --project_name 070617_01_53 --parameter_set reconstruction.cfg --events 250 --jobs 20 --dataset_cff cmssw_1_5_0_pre6_single_pi_pt_5_plus.cff
+./createGReconstructionProject --project_name 070617_01_54 --parameter_set reconstruction.cfg --events 250 --jobs 20 --dataset_cff cmssw_1_5_0_pre6_single_pi_pt_10_plus.cff
+./createGReconstructionProject --project_name 070617_01_55 --parameter_set reconstruction.cfg --events 250 --jobs 20 --dataset_cff cmssw_1_5_0_pre6_single_pi_pt_25_plus.cff
+./createGReconstructionProject --project_name 070617_01_56 --parameter_set reconstruction.cfg --events 250 --jobs 20 --dataset_cff cmssw_1_5_0_pre6_single_pi_pt_100_plus.cff
+
+./createGReconstructionProject --project_name 070617_01_61 --parameter_set reconstruction.cfg --events 100 --jobs 30 --dataset_cff cmssw_1_5_0_pre6_minbias.cff
+
+./createGReconstructionProject --project_name 070617_01_71 --parameter_set reconstruction.cfg --events 100 --jobs 30 --dataset_cff cmssw_1_5_0_pre6_bjets_120_170.cff
+./createGReconstructionProject --project_name 070617_01_72 --parameter_set reconstruction.cfg --events 100 --jobs 30 --dataset_cff cmssw_1_5_0_pre6_bjets_120_170_lowlumipileup.cff
+
+./createGReconstructionProject --project_name 070617_01_81 --parameter_set reconstruction.cfg --events 100 --jobs 30 --dataset_cff cmssw_1_5_0_pre6_ttbar.cff
+./createGReconstructionProject --project_name 070617_01_82 --parameter_set reconstruction.cfg --events 100 --jobs 30 --dataset_cff cmssw_1_5_0_pre6_ttbar_lowlumipileup.cff
