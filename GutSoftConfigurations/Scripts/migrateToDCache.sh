36d846d93a771b55e6aceeadd439b1b7fe0ca62b
gutsche <>
2007-07-10 16:44:49 +0000

    move from dccp to srmcp

--- GutSoftConfigurations/Scripts/migrateToDCache.sh
+++ GutSoftConfigurations/Scripts/migrateToDCache.sh
@@ -7,43 +7,39 @@
 for project in $*
 do
     echo ""
-    echo "Migrating project $project to /pnfs/cms/WAX/resilient/gutsche/condor/$project"
+    echo "Migrating project $project"
     echo ""
 
     echo "changing to $project"
     cd $project
     echo ""
 
-    echo "creating directory /pnfs/cms/WAX/resilient/gutsche/condor/$project"
-    mkdir -p /pnfs/cms/WAX/resilient/gutsche/condor/$project
-    echo ""
-
-    echo "copying all .root files to /pnfs/cms/WAX/resilient/gutsche/condor/$project"
+    echo "copying all .root files"
     echo ""
     for rootfile in `ls *.root 2>/dev/null`
     do
-	echo "copying $rootfile to /pnfs/cms/WAX/resilient/gutsche/condor/$project/$rootfile"
-	dccp $rootfile /pnfs/cms/WAX/resilient/gutsche/condor/$project/$rootfile
+	echo "copying $rootfile: srmcp file:////`pwd`/$rootfile srm://cmssrm.fnal.gov:8443/srm/managerv1?SFN=/resilient/gutsche/condor/$project/$rootfile"
+	srmcp file:////`pwd`/$rootfile srm://cmssrm.fnal.gov:8443/srm/managerv1?SFN=/resilient/gutsche/condor/$project/$rootfile
     done
     echo ""
 
-    echo "copying all .log.gz files to /pnfs/cms/WAX/resilient/gutsche/condor/$project"
+    echo "copying all .log.gz files"
     echo ""
     for loggzfile in `ls *.log.gz 2>/dev/null`
     do
-	echo "copying $loggzfile to /pnfs/cms/WAX/resilient/gutsche/condor/$project/$loggzfile"
-	dccp $loggzfile /pnfs/cms/WAX/resilient/gutsche/condor/$project/$loggzfile
+	echo "copying $loggzfile: srmcp file:////`pwd`/$loggzfile srm://cmssrm.fnal.gov:8443/srm/managerv1?SFN=/resilient/gutsche/condor/$project/$loggzfile"
+	srmcp file:////`pwd`/$loggzfile srm://cmssrm.fnal.gov:8443/srm/managerv1?SFN=/resilient/gutsche/condor/$project/$loggzfile
     done
     echo ""
 
-    echo "zipping and copying all .log files to /pnfs/cms/WAX/resilient/gutsche/condor/$project"
+    echo "zipping and copying all .log files"
     echo ""
     for logfile in `ls *.log 2>/dev/null`
     do
 	echo "zipping $logfile"
 	gzip $logfile
-	echo "copying $logfile to /pnfs/cms/WAX/resilient/gutsche/condor/$project/$logfile"
-	dccp $logfile /pnfs/cms/WAX/resilient/gutsche/condor/$project/$logfile
+	echo "copying $logfile.gz: srmcp file:////`pwd`/$logfile.gz srm://cmssrm.fnal.gov:8443/srm/managerv1?SFN=/resilient/gutsche/condor/$project/$logfile.gz"
+	srmcp file:////`pwd`/$logfile.gz srm://cmssrm.fnal.gov:8443/srm/managerv1?SFN=/resilient/gutsche/condor/$project/$logfile.gz
     done
     echo ""
     
