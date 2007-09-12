94a14863584a7bed9724344f8cceaeeb41347be1
dmytro <>
2007-09-12 02:39:25 +0000

    updated macros for pass2

--- CMS1/NtupleMacros/dilepton.cxx
+++ CMS1/NtupleMacros/dilepton.cxx
@@ -3,31 +3,59 @@
    gStyle->SetPalette(1);
    gStyle->SetOptStat(1111111);
    
-   TFile *fzmm = new TFile("/uscms_data/d1/cms1/V01-09-02-test/results/tablemaker_Zmumu_ntuple.root");
+   TFile *fzmm = new TFile("/uscms_data/d1/cms1/V01-09-06/results/tablemaker_Zmumu_ntuple.root");
    TTree *tzmm = (TTree*)(fzmm->Get("event"));
    
-   TFile *fzee = new TFile("/uscms_data/d1/cms1/V01-09-02-test/results/tablemaker_Zee_ntuple.root");
+   TFile *fzee = new TFile("/uscms_data/d1/cms1/V01-09-06/results/tablemaker_Zee_ntuple.root");
    TTree *tzee = (TTree*)(fzee->Get("event"));
    
-   TFile *fww = new TFile("/uscms_data/d1/cms1/V01-09-02-test/results/tablemaker_WW_ntuple.root");
+   TFile *fww = new TFile("/uscms_data/d1/cms1/V01-09-06/results/tablemaker_WW_ntuple.root");
    TTree *tww = (TTree*)(fww->Get("event"));
    
-   TFile *fwz = new TFile("/uscms_data/d1/cms1/V01-09-02-test/results/tablemaker_WZ_ntuple.root");
+   TFile *fwz = new TFile("/uscms_data/d1/cms1/V01-09-06/results/tablemaker_WZ_ntuple.root");
    TTree *twz = (TTree*)(fwz->Get("event"));
    
-   TFile *fzz = new TFile("/uscms_data/d1/cms1/V01-09-02-test/results/tablemaker_ZZ_ntuple.root");
+   TFile *fzz = new TFile("/uscms_data/d1/cms1/V01-09-06/results/tablemaker_ZZ_ntuple.root");
    TTree *tzz = (TTree*)(fzz->Get("event"));
    
-   TFile *fztt = new TFile("/uscms_data/d1/cms1/V01-09-02-test/results/tablemaker_Ztautau_ntuple.root");
+   TFile *fztt = new TFile("/uscms_data/d1/cms1/V01-09-06/results/tablemaker_Ztautau_ntuple.root");
    TTree *tztt = (TTree*)(fztt->Get("event"));
 
-   TFile *fhww = new TFile("/uscms_data/d1/cms1/V01-09-02-test/results/tablemaker_HWW_ntuple.root");
+   TFile *fhww = new TFile("/uscms_data/d1/cms1/V01-09-06/results/tablemaker_HWW_ntuple.root");
    TTree *thww = (TTree*)(fhww->Get("event"));
 
-   TFile *ftt = new TFile("/uscms_data/d1/cms1/V01-09-02-test/results/tablemaker_TTbar_ntuple.root");
+   TFile *ftt = new TFile("/uscms_data/d1/cms1/V01-09-06/results/tablemaker_TTbar_ntuple.root");
    TTree *ttt = (TTree*)(ftt->Get("event"));
 
    gROOT->LoadMacro("dilepton_functions.cxx");
+   
+   TCut emu_mue("(hyp_type==1||hyp_type==2)");
+   TCut pass1("((hyp_met>40)||(hyp_met>20&&(hyp_p4.mass()<80||hyp_p4.mass()>100)))");
+   TCut align_old("hyp_met/hyp_p4.pt()>0.6||fabs(fabs(hyp_metPhi-hyp_p4.phi())-3.1416)>0.25");
+   TCut align("hyp_met/hyp_p4.pt()>0.6||acos(cos(hyp_metPhi-hyp_p4.phi()-3.1416))>0.25");
+   TCut dyTight("hyp_met>30&&fabs(hyp_p4.M()-90)>10");
+   TCut dyLoose("hyp_met>30||(fabs(hyp_p4.M()-90)<10&&hyp_met>50)");
+   TCut iso("hyp_lt_iso<5&&hyp_ll_iso<5");
+
+   TCut iso("hyp_lt_iso<5&&hyp_ll_iso<5");
+   TCut align("hyp_met/hyp_p4.pt()>0.6||acos(cos(hyp_metPhi-hyp_p4.phi()-3.1416))>0.25");
+   TCut drellyan("hyp_met>30&&fabs(hyp_p4.M()-90)>10");
+   
+   TCut mue("hyp_type==1");
+   TCut emu("hyp_type==2");
+   TCut ee("hyp_type==3");
+   TCut mumu("hyp_type==0");
+
+   TCut pass2_mumu(mumu+iso+align+drellyan);
+   TCut pass2_ee(ee+iso+align+drellyan);
+   TCut pass2_mue(mue+iso+"hyp_met>20");
+   TCut pass2_emu(emu+iso+"hyp_met>20");
+   TCut pass2_ee_tight(ee+iso+align+drellyan+"elid_tightId[hyp_lt_index]==1");
+   TCut pass2_ee_loose(ee+iso+align+drellyan+"elid_looseId[hyp_lt_index]==1");
+   TCut pass2_mue_loose(mue+iso+"hyp_met>20"+"elid_looseId[hyp_ll_index]==1");
+   TCut pass2_mue_tight(mue+iso+"hyp_met>20"+"elid_tightId[hyp_ll_index]==1");
+   TCut pass2_emu_loose(emu+iso+"hyp_met>20"+"elid_looseId[hyp_lt_index]==1");
+   TCut pass2_emu_tight(emu+iso+"hyp_met>20"+"elid_tightId[hyp_lt_index]==1");
    
    cout << "Finished initialization." << endl;
 }
--- CMS1/NtupleMacros/dilepton_functions.cxx
+++ CMS1/NtupleMacros/dilepton_functions.cxx
@@ -29,64 +29,63 @@ TCanvas* plot(const char* draw_command, TCut cut)
    return c1;
 }
 
-TCanvas* plotStack(string draw_command, TCut cut, string title, string hname = "histo")
+TCanvas* plotStack(string draw_command, TCut cut, string title, string name = "histo")
 { 
-   c1 = new TCanvas("c1","c1",500,500);
-   THStack* stack = new THStack(hname.c_str(),title.c_str());
+   c1 = new TCanvas(name.c_str(),name.c_str(),500,500);
+   THStack* stack = new THStack(name.c_str(),title.c_str());
    legend = new TLegend(0.75,0.65,0.98,0.98,"Key");
 
    int color = 1;
    // Scales:
    // http://hepuser.ucsd.edu/twiki/bin/view/CMSPhysics/PassOnePlots
    // 
-   hww = new TH1F((hname+"_ww").c_str(),(hname+"_ww").c_str(),5,0,5);
-   tww->Draw((draw_command+">>"+hname+"_ww").c_str(), cut);
+   hww = new TH1F((name+"_ww").c_str(),(name+"_ww").c_str(),5,0,5);
+   tww->Draw((draw_command+">>"+name+"_ww").c_str(), cut);
    hww->SetFillColor(++color);
    // scale to 100 pb^-1
    hww->Scale(0.116);
    stack->Add(hww);
    legend->AddEntry(hww,"WW");
 
-   hwz = new TH1F((hname+"_wz").c_str(),(hname+"_wz").c_str(),5,0,5);
-   twz->Draw((draw_command+">>"+hname+"_wz").c_str(), cut);
+   hwz = new TH1F((name+"_wz").c_str(),(name+"_wz").c_str(),5,0,5);
+   twz->Draw((draw_command+">>"+name+"_wz").c_str(), cut);
    hwz->SetFillColor(++color);
    // scale to 100 pb^-1
    hwz->Scale(0.0526);
    stack->Add(hwz);
    legend->AddEntry(hwz,"WZ");
 
-   hzz = new TH1F((hname+"_zz").c_str(),(hname+"_zz").c_str(),5,0,5);
-   tzz->Draw((draw_command+">>"+hname+"_zz").c_str(), cut);
+   hzz = new TH1F((name+"_zz").c_str(),(name+"_zz").c_str(),5,0,5);
+   tzz->Draw((draw_command+">>"+name+"_zz").c_str(), cut);
    hzz->SetFillColor(++color);
    // scale to 100 pb^-1
    hzz->Scale(0.0496);
    stack->Add(hzz);
    legend->AddEntry(hzz,"ZZ");
 
-   htt = new TH1F((hname+"_tt").c_str(),(hname+"_tt").c_str(),5,0,5);
-   ttt->Draw((draw_command+">>"+hname+"_tt").c_str(), cut);
+   htt = new TH1F((name+"_tt").c_str(),(name+"_tt").c_str(),5,0,5);
+   ttt->Draw((draw_command+">>"+name+"_tt").c_str(), cut);
    htt->SetFillColor(++color);
    // scale to 100 pb^-1
    htt->Scale(0.113);
    stack->Add(htt);
    legend->AddEntry(htt,"TTbar");
    
-   hzee = new TH1F((hname+"_zee").c_str(),(hname+"_zee").c_str(),5,0,5);
-   tzee->Draw((draw_command+">>"+hname+"_zee").c_str(), cut);
+   hzee = new TH1F((name+"_zee").c_str(),(name+"_zee").c_str(),5,0,5);
+   tzee->Draw((draw_command+">>"+name+"_zee").c_str(), cut);
    hzee->SetFillColor(++color);
    // scale to 100 pb^-1
    hzee->Scale(5.15);
    stack->Add(hzee);
    legend->AddEntry(hzee,"Zee");
 
-   hzmm = new TH1F((hname+"_zmm").c_str(),(hname+"_zmm").c_str(),5,0,5);
-   tzmm->Draw((draw_command+">>"+hname+"_zmm").c_str(), cut);
+   hzmm = new TH1F((name+"_zmm").c_str(),(name+"_zmm").c_str(),5,0,5);
+   tzmm->Draw((draw_command+">>"+name+"_zmm").c_str(), cut);
    hzmm->SetFillColor(++color);
    // scale to 100 pb^-1
    hzmm->Scale(4.36);
    stack->Add(hzmm);
    legend->AddEntry(hzmm,"Zmumu");
-
    
    stack->Draw("hist");
    legend->Draw();
