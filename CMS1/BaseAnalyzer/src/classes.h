b390f4854dab8a5122317000d0433cb679d19f4f
dmytro <>
2007-11-10 21:45:12 +0000

    added dictionaries for data types used in our ntuples

--- CMS1/BaseAnalyzer/src/classes.h
+++ CMS1/BaseAnalyzer/src/classes.h
@@ -1,3 +1,29 @@
 // #include "CMS1/BaseAnalyzer/interface/BaseAnalyzer.h"
 // #include "CMS1/BaseAnalyzer/interface/BaseAnalyzerFWLite.h"
+#include <vector>
+#include "DataFormats/Math/interface/LorentzVector.h"
+#include "DataFormats/Common/interface/Wrapper.h"
 
+namespace {
+   namespace {
+      std::vector<std::vector<int> > vi2d;
+      edm::Wrapper<std::vector<std::vector<int> > > wvi2d;
+	
+      std::vector<std::vector<std::vector<int> > > vi3d;
+      edm::Wrapper<std::vector<std::vector<std::vector<int> > > > wvi3d;
+      
+      std::vector<std::vector<float> > vf2d;
+      edm::Wrapper<std::vector<std::vector<float> > > wvf2d;
+      
+      std::vector<std::vector<std::vector<float> > > vf3d;
+      edm::Wrapper<std::vector<std::vector<std::vector<float> > > > wvf3d;
+      
+      std::vector<std::vector<math::XYZTLorentzVector> > vp2d;
+      edm::Wrapper<std::vector<std::vector<math::XYZTLorentzVector> > > wvp2d;
+      
+      std::vector<std::vector<std::vector<math::XYZTLorentzVector> > > vp3d;
+      edm::Wrapper<std::vector<std::vector<std::vector<math::XYZTLorentzVector> > > > wvp3d;
+   }
+}
+
+  
