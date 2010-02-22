9b68b2f132501e0a839904c1ed38b6afcc8becd9
gutsche <>
2010-02-22 08:24:28 +0000

    excluded /store/user and backfill and deactivated phedex and dbs checks

--- GutSoftConfigurations/Scripts/FNAL/parseLFNList.py
+++ GutSoftConfigurations/Scripts/FNAL/parseLFNList.py
@@ -8,6 +8,10 @@ datasets = {}
 
 for line in input.readlines() :
     if line.count('/store') > 0 :
+        if line.count('/store/user') > 0 :
+            continue
+        if line.count('backfill') > 0 :
+            continue
         array = line.split('/')
         dataset = '/'.join(array[0:7])
         if dataset in datasets.keys():
@@ -35,30 +39,30 @@ for index in range(10):
         os.system(command)
         print files[sorted[index]],sorted[index],filelist
 
-for filelist in filelists:
-    dbslist = filelist + '.dbs'
-    phedexlist = filelist + '.phedex'
-    phedexoutput = open(phedexlist,'w')
-    files = open(filelist,'r').readlines()
-    for file in files:
-        if file.count('/store/') > 0 :
+## for filelist in filelists:
+##     dbslist = filelist + '.dbs'
+##     phedexlist = filelist + '.phedex'
+##     phedexoutput = open(phedexlist,'w')
+##     files = open(filelist,'r').readlines()
+##     for file in files:
+##         if file.count('/store/') > 0 :
             
-            command = 'python $DBSCMD_HOME/dbsCommandLine.py -c search --query=\"find dataset,dataset.status,file where file = ' + file.strip() + ' and site=*.fnal.gov\" --noheader > ' + dbslist
-            os.system(command)
+##             command = 'python $DBSCMD_HOME/dbsCommandLine.py -c search --query=\"find dataset,dataset.status,file where file = ' + file.strip() + ' and site=*.fnal.gov\" --noheader > ' + dbslist
+##             os.system(command)
 
-            url = 'http://cmsweb.cern.ch/phedex/datasvc/json/prod/filereplicas?lfn=' + file.strip()
-            result = json.load(urllib.urlopen(url))
+##             url = 'http://cmsweb.cern.ch/phedex/datasvc/json/prod/filereplicas?lfn=' + file.strip()
+##             result = json.load(urllib.urlopen(url))
 
-            nodes = []
+##             nodes = []
 
-            for block in result['phedex']['block']:
-                for filelist in block['file'] :
-                    for replica in filelist['replica']:
-                        if replica['node'].count('FNAL') > 0 and replica['node'] not in nodes :
-                            nodes.append(replica['node'])
-            if len(nodes) > 0 :
-                outputstring = file.strip() + " " + ','.join(nodes) + "\n"
-                phedexoutput.write(outputstring)
+##             for block in result['phedex']['block']:
+##                 for filelist in block['file'] :
+##                     for replica in filelist['replica']:
+##                         if replica['node'].count('FNAL') > 0 and replica['node'] not in nodes :
+##                             nodes.append(replica['node'])
+##             if len(nodes) > 0 :
+##                 outputstring = file.strip() + " " + ','.join(nodes) + "\n"
+##                 phedexoutput.write(outputstring)
 
-    phedexoutput.close()
+##     phedexoutput.close()
 
