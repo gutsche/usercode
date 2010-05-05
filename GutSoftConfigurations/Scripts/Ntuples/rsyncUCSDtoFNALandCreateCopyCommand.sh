8500a3be7f5a5cf70d1ab256771fd196adc6ae07
gutsche <>
2010-05-05 19:28:02 +0000

    latest chagnges

--- GutSoftConfigurations/Scripts/Ntuples/rsyncUCSDtoFNALandCreateCopyCommand.sh
+++ GutSoftConfigurations/Scripts/Ntuples/rsyncUCSDtoFNALandCreateCopyCommand.sh
@@ -3,4 +3,6 @@
 # execute in /storage/local/data2/cms1/cms2
 # clean /pnfs/cms/WAX/resilient/gutsche
 rsync -n -a -i --ignore-existing uaf-3.t2.ucsd.edu:/data/tmp/cms2/ . | grep merge | awk '{print $2}' | awk '{print "lcg-cp -v -n 20 file:////`pwd`/"$1" srm://cmssrm.fnal.gov:8443/srm/managerv2?SFN=/resilient/gutsche/"$1}'
+#rsync -n -a -i --ignore-existing uaf-3.t2.ucsd.edu:/nfs-3/userdata/cms2/ . | grep merge | awk '{print $2}' | awk '{print "lcg-cp -v -n 20 file:////`pwd`/"$1" srm://cmssrm.fnal.gov:8443/srm/managerv2?SFN=/resilient/gutsche/"$1}'
+#rsync -n -a -i --ignore-existing uaf-5.t2.ucsd.edu:/data/tmp/fgolf/ . | grep merge | awk '{print $2}' | awk '{print "lcg-cp -v -n 20 file:////`pwd`/"$1" srm://cmssrm.fnal.gov:8443/srm/managerv2?SFN=/resilient/gutsche/"$1}'
 # copy output to uaf-3 and execute in /data/tmp/cms2
