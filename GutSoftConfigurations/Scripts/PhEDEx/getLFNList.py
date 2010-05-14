9b38ba5d6b2f2f402bfff1cf0dacd0dff55db0ca
gutsche <>
2010-05-14 17:51:42 +0000

    add new script which queries PhEDEx to get a list of flies for a dataset

---
+++ GutSoftConfigurations/Scripts/PhEDEx/getLFNList.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import sys,os,urllib,json,getopt
+
+datasetpath = None
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "", ["dataset="])
+except getopt.GetoptError:
+    print 'Please specify dataset with --dataset'
+    sys.exit(2)
+
+# check command line parameter
+for opt, arg in opts :
+    if opt == "--dataset" :
+        datasetpath = arg
+        
+if datasetpath == None:
+    print 'Please specify dataset with --dataset'
+    sys.exit(2)
+            
+url = 'http://cmsweb.cern.ch/phedex/datasvc/json/prod/filereplicas?block=' + datasetpath + '*'
+result = json.load(urllib.urlopen(url))
+
+for block in result['phedex']['block']:
+        for filelist in block['file'] :
+            print filelist['name']
+
