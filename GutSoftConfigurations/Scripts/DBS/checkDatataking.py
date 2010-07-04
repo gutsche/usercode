7310ee012c273225dde67ba1c49c228f6e3df6b5
gutsche <>
2010-07-04 21:53:16 +0000

    add data taking status checking script and environment setup

---
+++ GutSoftConfigurations/Scripts/DBS/checkDatataking.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python
+
+import sys, os, datetime, subprocess, shlex, tempfile, json
+
+startdate = datetime.date.today() - datetime.timedelta(days=1)
+startdatestring = startdate.strftime("%Y-%m-%d")
+
+rawdataset = '/JetMETTau/Run2010A-v1/RAW'
+recodataset = '/JetMETTau/Run2010A-PromptReco-v4/RECO'
+skimdatasets = ['/JetMETTau/Run2010A-CS_DiJetAve-v6/USER','/JetMETTau/Run2010A-CS_Tau-v6/RAW-RECO']
+
+def queryForRunsAndTime(commandline):
+    args = shlex.split(commandline)
+    output = subprocess.Popen(args, shell=False, stdout=subprocess.PIPE)
+
+    runs = {}
+
+    for line in output.communicate()[0].split('\n') :
+        if len(line) > 0 :
+            array = line.split()
+            run = array[0]
+            date = ' '.join(array[1:])
+            runs[run] = date
+    
+    return runs
+
+# collect run selection for last day
+commandline = "dbs search --query=\"find run,run.createdate where dataset = " + rawdataset + " and run.createdate > " + startdatestring + "\" --noheader"
+runs = queryForRunsAndTime(commandline)
+
+sorted_runs = runs.keys()
+sorted_runs.sort()
+run_selection_string = "(run = " + ' or run = '.join(sorted_runs) + ')'
+all_runs = sorted_runs
+
+print ''
+print 'Runs created since',startdatestring,'in dataset',rawdataset
+print ''
+print '\n'.join(sorted_runs)
+
+
+# raw runs at FNAL
+commandline = "dbs search --query=\"find run,run.createdate where dataset = " + rawdataset + " and " + run_selection_string + " and site = *fnal.gov \" --noheader"
+runs = queryForRunsAndTime(commandline)
+
+sorted_runs = runs.keys()
+sorted_runs.sort()
+
+print ''
+print 'Runs created since',startdatestring,'in dataset',rawdataset,'at FNAL'
+print ''
+print '\n'.join(sorted_runs)
+
+# reco runs at FNAL
+commandline = "dbs search --query=\"find run,run.createdate where dataset = " + recodataset + " and " + run_selection_string + " and site = *fnal.gov \" --noheader"
+runs = queryForRunsAndTime(commandline)
+
+sorted_runs = runs.keys()
+sorted_runs.sort()
+
+print ''
+print 'Runs created since',startdatestring,'in dataset',recodataset,'at FNAL'
+print ''
+print '\n'.join(sorted_runs)
+
+# skim runs at FNAL
+for skimdataset in skimdatasets :
+    commandline = "dbs search --query=\"find run,run.createdate where dataset = " + skimdataset + " and " + run_selection_string + " and site = *fnal.gov \" --noheader"
+    runs = queryForRunsAndTime(commandline)
+
+    sorted_runs = runs.keys()
+    sorted_runs.sort()
+
+    print ''
+    print 'Runs created since',startdatestring,'in dataset',skimdataset,'at FNAL'
+    print ''
+    print '\n'.join(sorted_runs)
+
+# query for lumi
+
+# repare lumi json for runs
+json_runs = {}
+for run in all_runs :
+    json_runs[run] = [[1,999999]]
+    
+tmp = tempfile.mkstemp()
+tmp_handle = open(tmp[1],'w')
+
+json.dump(json_runs,tmp_handle)
+tmp_handle.close()
+
+commandline = 'lumiCalc.py -c frontier://LumiProd/CMS_LUMI_PROD -i ' + tmp[1] + ' overview'
+args = shlex.split(commandline)
+output = subprocess.Popen(args, shell=False, stdout=subprocess.PIPE)
+
+print ''
+print 'Lumi corresponding to all lumi sections in all runs created since',startdatestring,'in dataset',rawdataset
+print ''
+
+print output.communicate()[0]
+print ''
+    
+os.remove(tmp[1])
