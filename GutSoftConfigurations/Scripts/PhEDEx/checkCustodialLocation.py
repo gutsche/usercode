037e9500056f59e85e58c424aa50bc05915b342a
gutsche <>
2010-09-05 22:20:01 +0000

    multiple changes

--- GutSoftConfigurations/Scripts/PhEDEx/checkCustodialLocation.py
+++ GutSoftConfigurations/Scripts/PhEDEx/checkCustodialLocation.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python
 
-import sys,getopt
-import urllib
-from xml.dom import minidom
+import sys,getopt,urllib,json
 
 datasetpath = None
+lfn = None
 allSites = 0
 try:
-    opts, args = getopt.getopt(sys.argv[1:], "", ["dataset=","allSites"])
+    opts, args = getopt.getopt(sys.argv[1:], "", ["lfn=","dataset=","allSites"])
 except getopt.GetoptError:
     print 'Please specify dataset with --dataset'
     print 'Specify --allSites to show all site location, otherwise show only T1 sites'
@@ -17,46 +16,68 @@ except getopt.GetoptError:
 for opt, arg in opts :
     if opt == "--dataset" :
         datasetpath = arg
+    if opt == "--lfn" :
+        lfn = arg
     if opt == "--allSites" :
         allSites = 1
         
-if datasetpath == None:
+if datasetpath == None and lfn == None:
     print 'Please specify dataset with --dataset'
     sys.exit(2)
 
-url='http://cmsweb.cern.ch/phedex/datasvc/xml/prod/blockreplicas?block=' + datasetpath + '*'
-input = urllib.urlopen(url)
-
-xmldoc = minidom.parse(input)
 
 custodial = {}
 non_custodial = {}
 
-for phedex in xmldoc.childNodes :
-    for block in phedex.childNodes:
-        name = block.attributes['name'].value
-        for replica in block.childNodes:
-            site = replica.attributes['node'].value
-            files = replica.attributes['files'].value
-##             if site[-4:] == '_MSS':
-##                 site = site[:-4]
-##             if site[-7:] == '_Buffer':
-##                 site = site[:-7]
-##             if site[-7:] == '_Export':
-##                 site = site[:-7]
-            if allSites == 1 or ( site[0:2] == 'T1' or site[0:2] == 'T0') :
-                if replica.attributes['custodial'].value == 'y':
-                    if site not in custodial.keys() :
-                        custodial[site] = int(files)
+if lfn == None :
+    url='http://cmsweb.cern.ch/phedex/datasvc/json/prod/blockreplicas?block=' + datasetpath + '*'
+    result = json.load(urllib.urlopen(url))
+    try:
+        for block in result['phedex']['block']:
+            name = block['name']
+            for replica in block['replica']:
+                files = replica['files']
+                site = str(replica['node'])
+                is_custodial = replica['custodial']
+                if allSites == 1 or ( site[0:2] == 'T1' or site[0:2] == 'T0') :
+                    if is_custodial == 'y' :
+                        if site not in custodial.keys() :
+                            custodial[site] = int(files)
+                        else :
+                            custodial[site] += int(files)
                     else :
-                        custodial[site] += int(files)
-                else :
-                    if site not in non_custodial.keys() :
-                        non_custodial[site] = int(files)
-                    else :
-                        non_custodial[site] += int(files)
-
-
+                        if site not in non_custodial.keys() :
+                            non_custodial[site] = int(files)
+                        else :
+                            non_custodial[site] += int(files)
+    except:
+        print 'problems with dataset:',dataset
+else:
+    url='http://cmsweb.cern.ch/phedex/datasvc/json/prod/filereplicas?lfn=' + lfn
+    result = json.load(urllib.urlopen(url))
+    try:
+        for block in result['phedex']['block']:
+            name = block['name']
+            files = block['files']
+            for fileinstance in block['file'] :
+                if fileinstance['name'] == lfn :
+                    for replica in fileinstance['replica']:
+                        site = str(replica['node'])
+                        is_custodial = replica['custodial']
+                        if allSites == 1 or ( site[0:2] == 'T1' or site[0:2] == 'T0') :
+                            if is_custodial == 'y' :
+                                if site not in custodial.keys() :
+                                    custodial[site] = int(files)
+                                else :
+                                    custodial[site] += int(files)
+                            else :
+                                if site not in non_custodial.keys() :
+                                    non_custodial[site] = int(files)
+                                else :
+                                    non_custodial[site] += int(files)
+    except:
+        print 'problems with lfn:',lfn
+        
 custodial_sites = custodial.keys()
 non_custodial_sites = non_custodial.keys()
 
@@ -85,4 +106,7 @@ for custsite in custodial_sites :
 if custsites[-1:] == ',' :
     custsites = custsites[:-1]
 
-print 'dataset:',datasetpath,'custodial:',custsites,'non-custodial:',sites
+if lfn == None :
+    print 'dataset:',datasetpath,'custodial:',custsites,'non-custodial:',sites
+else :
+    print 'lfn:',lfn,'custodial:',custsites,'non-custodial:',sites
