7e106b9cd82dd031912e27d44877fde37cfe4aa8
gutsche <>
2010-10-07 03:09:59 +0000

    new script to calculate lumi in wildcard dataset name

---
+++ GutSoftConfigurations/Scripts/DBS/calculateLumi.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+
+import sys, os, datetime, subprocess, shlex, tempfile, json, getopt
+
+opts, args = getopt.getopt(sys.argv[1:], "", ["dataset="])
+
+dataset = None
+
+for opt, arg in opts :
+    if opt == "--dataset" :
+        dataset = arg
+        
+if dataset == None :
+    print ''
+    print 'Please define dataset to query for with --dataset'
+    print ''
+
+def queryForRunsAndTime(commandline):
+    args = shlex.split(commandline)
+    output = subprocess.Popen(args, shell=False, stdout=subprocess.PIPE)
+
+    runs = {}
+
+    for line in output.communicate()[0].split('\n') :
+        if len(line) > 0 :
+            array = line.split()
+            run = array[0]
+            date = ' '.join(array[1:])
+            runs[run] = date
+    
+    return runs
+
+# collect run selection for last day
+commandline = "dbs search --production --noheader --query=\"find dataset where dataset = " + dataset + " and dataset != *preprod* \""
+args = shlex.split(commandline)
+output = subprocess.Popen(args, shell=False, stdout=subprocess.PIPE)
+print ''
+print 'Datasets:'
+print ''
+print output.communicate()[0]
+
+commandline = "dbs search --production --query=\"find run,run.createdate where dataset = " + dataset + " and dataset != *preprod* \" --noheader"
+runs = queryForRunsAndTime(commandline)
+
+sorted_runs = runs.keys()
+sorted_runs.sort()
+all_runs = sorted_runs
+
+# query for lumi
+
+# repare lumi json for runs
+json_runs = {}
+for run in all_runs :
+    json_runs[run] = [[1,999999]]
+    
+tmp = tempfile.mkstemp()
+tmp_handle = open(tmp[1],'w')
+
+json.dump(json_runs,tmp_handle)
+tmp_handle.close()
+
+commandline = 'lumiCalc.py -c frontier://LumiProd/CMS_LUMI_PROD -i ' + tmp[1] + ' overview'
+print ''
+print 'lumiCalc commandline:'
+print commandline
+
+args = shlex.split(commandline)
+output = subprocess.Popen(args, shell=False, stdout=subprocess.PIPE)
+
+print ''
+print 'Lumi corresponding to all lumi sections in all runs in dataset',dataset
+print ''
+
+print output.communicate()[0]
+print ''
+    
+# os.remove(tmp[1])
