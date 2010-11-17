7873ee4f91e479a26ffc05de3d6a3e79f587a82a
gutsche <>
2010-11-17 09:31:54 +0000

    updated for blocks

--- GutSoftConfigurations/Scripts/DBS/checkDeleteRequest.py
+++ GutSoftConfigurations/Scripts/DBS/checkDeleteRequest.py
@@ -20,6 +20,8 @@ if requestID == None:
     sys.exit(2)
 
 datasets = []
+blocks = []
+
 
 url='https://cmsweb.cern.ch/phedex/datasvc/json/prod/deleterequests?request=' + requestID
 result = json.load(urllib.urlopen(url))
@@ -27,73 +29,148 @@ result = json.load(urllib.urlopen(url))
 for item in result['phedex']['request']:
     for dataset in item['data']['dbs']['dataset']:
         datasets.append(dataset['name'])
+    for block in item['data']['dbs']['block']:
+        blocks.append(block['name'])
         
-for dataset in datasets:
-    custodial = {}
-    non_custodial = {}
-    status = ''
+if len(datasets) > 0 :
+    for dataset in datasets:
+        custodial = {}
+        non_custodial = {}
+        status = ''
     
-    commandline = "dbs search --query=\"find dataset.status where dataset = " + str(dataset) + "\" --noheader --production"
-    localargs = shlex.split(commandline)
-    output = subprocess.Popen(localargs, shell=False, stdout=subprocess.PIPE)
+        commandline = "dbs search --query=\"find dataset.status where dataset = " + str(dataset) + "\" --noheader --production"
+        localargs = shlex.split(commandline)
+        output = subprocess.Popen(localargs, shell=False, stdout=subprocess.PIPE)
     
-    lines = output.communicate()[0].split('\n')
-    if len(lines) != 2 :
-        print 'Dataset:',dataset,'with multiple status entries, aborting'
-        sys.exit(1)
-    for line in lines:
-        if len(line) > 0 :
-             status = line.strip()
+        lines = output.communicate()[0].split('\n')
+        if len(lines) != 2 :
+            print 'Dataset:',dataset,'with multiple status entries, aborting'
+            sys.exit(1)
+        for line in lines:
+            if len(line) > 0 :
+                 status = line.strip()
 
-    url='https://cmsweb.cern.ch/phedex/datasvc/json/prod/blockreplicas?block=' + dataset + '*'
-    result = json.load(urllib.urlopen(url))
-    try:
-        for block in result['phedex']['block']:
-            name = block['name']
-            for replica in block['replica']:
-                files = replica['files']
-                site = str(replica['node'])
-                is_custodial = replica['custodial']
-                if allSites == 1 or ( site[0:2] == 'T1' or site[0:2] == 'T0') :
-                    if is_custodial == 'y' :
-                        if site not in custodial.keys() :
-                            custodial[site] = int(files)
+        url='https://cmsweb.cern.ch/phedex/datasvc/json/prod/blockreplicas?block=' + dataset + '*'
+        result = json.load(urllib.urlopen(url))
+        try:
+            for block in result['phedex']['block']:
+                name = block['name']
+                for replica in block['replica']:
+                    files = replica['files']
+                    site = str(replica['node'])
+                    is_custodial = replica['custodial']
+                    if allSites == 1 or ( site[0:2] == 'T1' or site[0:2] == 'T0') :
+                        if is_custodial == 'y' :
+                            if site not in custodial.keys() :
+                                custodial[site] = int(files)
+                            else :
+                                custodial[site] += int(files)
                         else :
-                            custodial[site] += int(files)
-                    else :
-                        if site not in non_custodial.keys() :
-                            non_custodial[site] = int(files)
-                        else :
-                            non_custodial[site] += int(files)
-    except:
-        print 'problems with dataset:',dataset
+                            if site not in non_custodial.keys() :
+                                non_custodial[site] = int(files)
+                            else :
+                                non_custodial[site] += int(files)
+        except:
+            print 'problems with dataset:',dataset
         
-    custodial_sites = custodial.keys()
-    non_custodial_sites = non_custodial.keys()
+        custodial_sites = custodial.keys()
+        non_custodial_sites = non_custodial.keys()
 
-    custodial_sites.sort()
-    non_custodial_sites.sort()
+        custodial_sites.sort()
+        non_custodial_sites.sort()
 
 
-    if len(custodial_sites) == 0 :
-        custodial['NONE'] = 0
-    if len(non_custodial_sites) == 0 :
-        non_custodial['NONE'] = 0
-    if len(custodial_sites) == 1 and custodial_sites[0].count('T0') > 0 :
-        custodial['NONE'] = 0
-    if len(non_custodial_sites) == 1 and non_custodial_sites[0].count('T0') > 0 :
-        non_custodial['NONE'] = 0
+        if len(custodial_sites) == 0 :
+            custodial['NONE'] = 0
+        if len(non_custodial_sites) == 0 :
+            non_custodial['NONE'] = 0
+        if len(custodial_sites) == 1 and custodial_sites[0].count('T0') > 0 :
+            custodial['NONE'] = 0
+        if len(non_custodial_sites) == 1 and non_custodial_sites[0].count('T0') > 0 :
+            non_custodial['NONE'] = 0
 
-    sites = ''
-    custsites = ''
-    for site in non_custodial_sites :
-        if site not in custodial_sites :
-            sites = sites + site + '(' + str(non_custodial[site]) + '),'
-    if sites[-1:] == ',' :
-        sites = sites[:-1]
-    for custsite in custodial_sites :
-        custsites = custsites + custsite + '(' + str(custodial[custsite]) + '),'
-    if custsites[-1:] == ',' :
-        custsites = custsites[:-1]
+        sites = ''
+        custsites = ''
+        for site in non_custodial_sites :
+            if site not in custodial_sites :
+                sites = sites + site + '(' + str(non_custodial[site]) + '),'
+        if sites[-1:] == ',' :
+            sites = sites[:-1]
+        for custsite in custodial_sites :
+            custsites = custsites + custsite + '(' + str(custodial[custsite]) + '),'
+        if custsites[-1:] == ',' :
+            custsites = custsites[:-1]
 
-    print 'dataset:',dataset,'DBS status:',status,'custodial:',custsites,'non-custodial:',sites
+        print 'dataset:',dataset,'DBS status:',status,'custodial:',custsites,'non-custodial:',sites
+
+if len(blocks) > 0 :
+    for block in blocks:
+        custodial = {}
+        non_custodial = {}
+        status = ''
+
+        commandline = "dbs search --query=\"find dataset.status where block = " + str(block) + "\" --noheader --production"
+        localargs = shlex.split(commandline)
+        output = subprocess.Popen(localargs, shell=False, stdout=subprocess.PIPE)
+
+        lines = output.communicate()[0].split('\n')
+        if len(lines) != 2 :
+            print 'Dataset:',dataset,'with multiple status entries, aborting'
+            sys.exit(1)
+        for line in lines:
+            if len(line) > 0 :
+                 status = line.strip()
+
+        url='https://cmsweb.cern.ch/phedex/datasvc/json/prod/blockreplicas?block=' + block
+        url = url.replace('#','%23')
+        result = json.load(urllib.urlopen(url))
+        try:
+            for item in result['phedex']['block']:
+                name = item['name']
+                for replica in item['replica']:
+                    files = replica['files']
+                    site = str(replica['node'])
+                    is_custodial = replica['custodial']
+                    if allSites == 1 or ( site[0:2] == 'T1' or site[0:2] == 'T0') :
+                        if is_custodial == 'y' :
+                            if site not in custodial.keys() :
+                                custodial[site] = int(files)
+                            else :
+                                custodial[site] += int(files)
+                        else :
+                            if site not in non_custodial.keys() :
+                                non_custodial[site] = int(files)
+                            else :
+                                non_custodial[site] += int(files)
+        except:
+            print 'problems with dataset:',dataset
+
+        custodial_sites = custodial.keys()
+        non_custodial_sites = non_custodial.keys()
+
+        custodial_sites.sort()
+        non_custodial_sites.sort()
+
+
+        if len(custodial_sites) == 0 :
+            custodial['NONE'] = 0
+        if len(non_custodial_sites) == 0 :
+            non_custodial['NONE'] = 0
+        if len(custodial_sites) == 1 and custodial_sites[0].count('T0') > 0 :
+            custodial['NONE'] = 0
+        if len(non_custodial_sites) == 1 and non_custodial_sites[0].count('T0') > 0 :
+            non_custodial['NONE'] = 0
+
+        sites = ''
+        custsites = ''
+        for site in non_custodial_sites :
+            if site not in custodial_sites :
+                sites = sites + site + '(' + str(non_custodial[site]) + '),'
+        if sites[-1:] == ',' :
+            sites = sites[:-1]
+        for custsite in custodial_sites :
+            custsites = custsites + custsite + '(' + str(custodial[custsite]) + '),'
+        if custsites[-1:] == ',' :
+            custsites = custsites[:-1]
+
+        print 'block:',block,'DBS status:',status,'custodial:',custsites,'non-custodial:',sites
