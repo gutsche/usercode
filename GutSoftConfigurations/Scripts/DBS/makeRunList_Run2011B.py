3731fa355225075b0d3f2644c88ee4a2d5094209
gutsche <>
2011-10-05 22:43:16 +0000

    minor changes

---
+++ GutSoftConfigurations/Scripts/DBS/makeRunList_Run2011B.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+
+import sys, os, datetime, subprocess, shlex, tempfile, getopt, json
+
+query_days = 1
+query_run = None
+min_events = 100
+
+opts, args = getopt.getopt(sys.argv[1:], "", ["days=","run=","min_events="])
+
+for opt, arg in opts :
+    if opt == "--days" :
+        query_days = int(arg)
+    if opt == "--min_events" :
+        min_events = int(arg)
+    if opt == "--run" :
+        query_run = arg
+        
+if query_days != 1 :
+    print ''
+    print 'Query for',query_days,'days'
+    print ''
+
+if query_run != None :
+    print ''
+    print 'Query for run',query_run
+    print ''
+
+
+rawdatasets = [
+'/DoubleElectron/Run2011B-v1/RAW',
+'/DoubleMu/Run2011B-v1/RAW',
+# '/ElectronHad/Run2011B-v1/RAW',
+# '/ForwardTriggers/Run2011B-v1/RAW',
+# '/HT/Run2011B-v1/RAW',
+'/Jet/Run2011B-v1/RAW',
+# '/METBTag/Run2011B-v1/RAW',
+# '/MinimumBias/Run2011B-v1/RAW',
+# '/MuEG/Run2011B-v1/RAW',
+# '/MuHad/Run2011B-v1/RAW',
+# '/MuOnia/Run2011B-v1/RAW',
+# '/MultiJet/Run2011B-v1/RAW',
+# '/Photon/Run2011B-v1/RAW',
+# '/PhotonHad/Run2011B-v1/RAW',
+'/SingleElectron/Run2011B-v1/RAW',
+'/SingleMu/Run2011B-v1/RAW',
+# '/Tau/Run2011B-v1/RAW',
+# '/TauPlusX/Run2011B-v1/RAW',
+]
+
+def queryCommandline(runs,commandline):
+    print commandline
+    args = shlex.split(commandline)
+    output = subprocess.Popen(args, shell=False, stdout=subprocess.PIPE)
+    for line in output.communicate()[0].split('\n') :
+        if len(line) > 0 :
+            array = line.split()
+            run = int(array[0])
+            events = int(array[1])
+            date = ' '.join(array[2:])
+            print 'dataset:',dataset,'run:',run,'events:',events
+            if run > 99999 and run < 1000000 and events > min_events:
+                if run in runs.keys() :
+                    runs[run].append(dataset)
+                else:
+                    runs[run] = [dataset]
+    return
+
+runs = {}
+# query DBS
+for dataset in rawdatasets :
+    # collect run selection for last day
+    if query_run == None :
+        for day in range(query_days) :
+            enddate = datetime.date.today() - datetime.timedelta(days=day)
+            enddatestring = enddate.strftime("%Y-%m-%d")
+            startdate = datetime.date.today() - datetime.timedelta(days=day+1)
+            startdatestring = startdate.strftime("%Y-%m-%d")
+            commandline = "dbs search --query=\"find run,sum(block.numevents),run.createdate where run.createdate >= " + startdatestring + " and run.createdate < " + enddatestring + " and dataset = " + dataset + "\" --noheader"
+            queryCommandline(runs,commandline)
+    else :
+        commandline = "dbs search --query=\"find run,sum(block.numevents),run.createdate where run = " + str(query_run) + " and dataset = " + dataset + "\" --noheader"
+        queryCommandline(runs,commandline)
+
+# check that all RAW datasets have events > min_events for a run
+results = []
+for run in runs.keys():
+    enough_events = True
+    for dataset in rawdatasets:
+        if dataset not in runs[run] :
+            enough_events = False
+    if enough_events == True :
+        results.append(run)
+        
+results.sort()
+
+print results
+
+enddate = datetime.date.today()
+startdate = datetime.date.today() - datetime.timedelta(days=query_days)
+
+output_file_name = "runs_" + str(results[0]) + "_" + str(results[-1]) + ".json"
+
+tmp_handle = open(output_file_name,'w')
+
+json.dump(results,tmp_handle)
+tmp_handle.close()
--- GutSoftConfigurations/Scripts/DBS/mc_statistics_2011.sh
+++ GutSoftConfigurations/Scripts/DBS/mc_statistics_2011.sh
@@ -159,19 +159,37 @@
 # cat mc_2011_jul_reco.blocks | grep -v '/CMSSW_' | grep -vi test | grep -vi preprod | grep -v Backfill | grep -vi jobrobot | grep -vi sam | grep -v X0M | grep -v HCal | grep -v Flat | grep -v IDEAL   | grep -v NoPU | awk '{SUM1+=$2} {SUM2+=$3} END {print "July 2011 GEN-SIM-RECO: Size [TiB]: "SUM1/1024/1024/1024/1024" Events: "SUM2}'
 # cat mc_2011_jul_aod.blocks | grep -v '/CMSSW_' | grep -vi test | grep -vi preprod | grep -v Backfill | grep -vi jobrobot | grep -vi sam | grep -v X0M | grep -v HCal | grep -v Flat | grep -v IDEAL | grep -v NoPU | grep -v Spring11 | awk '{SUM1+=$2} {SUM2+=$3} END {print "July 2011 AODSIM: Size [TiB]: "SUM1/1024/1024/1024/1024" Events: "SUM2}'
 
+# echo ""
+# echo "RAW categories:"
+# echo ""
+# cat mc_2011_aug_raw.blocks | grep -v '/CMSSW_' | grep -vi test | grep -vi preprod | grep -v Backfill | grep -vi jobrobot | grep -vi sam | grep -v GEN-SIM-RAW | awk -F\/ '{print $3}' | sort -u
+# echo ""
+# echo "RECO categories:"
+# echo ""
+# cat mc_2011_aug_reco.blocks | grep -v '/CMSSW_' | grep -vi test | grep -vi preprod | grep -v Backfill | grep -vi jobrobot | grep -vi sam | grep -v X0M | grep -v HCal | grep -v Flat | grep -v IDEAL  | grep -v NoPU | awk -F\/ '{print $3}' | sort -u
+# echo ""
+# echo "AOD categories:"
+# echo ""
+# cat mc_2011_aug_aod.blocks | grep -v '/CMSSW_' | grep -vi test | grep -vi preprod | grep -v Backfill | grep -vi jobrobot | grep -vi sam | grep -v X0M | grep -v HCal | grep -v Flat | grep -v IDEAL  | grep -v NoPU | grep -v Spring11 | awk -F\/ '{print $3}' | sort -u
+# 
+# cat mc_2011_aug_raw.blocks | grep -v '/CMSSW_' | grep -vi test | grep -vi preprod | grep -v Backfill | grep -vi jobrobot | grep -vi sam | grep -v GEN-SIM-RAW | awk '{SUM1+=$2} {SUM2+=$3} END {print "August 2011 GEN-SIM-RAW: Size [TiB]: "SUM1/1024/1024/1024/1024" Events: "SUM2}'
+# cat mc_2011_aug_reco.blocks | grep -v '/CMSSW_' | grep -vi test | grep -vi preprod | grep -v Backfill | grep -vi jobrobot | grep -vi sam | grep -v X0M | grep -v HCal | grep -v Flat | grep -v IDEAL   | grep -v NoPU | awk '{SUM1+=$2} {SUM2+=$3} END {print "August 2011 GEN-SIM-RECO: Size [TiB]: "SUM1/1024/1024/1024/1024" Events: "SUM2}'
+# cat mc_2011_aug_aod.blocks | grep -v '/CMSSW_' | grep -vi test | grep -vi preprod | grep -v Backfill | grep -vi jobrobot | grep -vi sam | grep -v X0M | grep -v HCal | grep -v Flat | grep -v IDEAL | grep -v NoPU | grep -v Spring11 | awk '{SUM1+=$2} {SUM2+=$3} END {print "August 2011 AODSIM: Size [TiB]: "SUM1/1024/1024/1024/1024" Events: "SUM2}'
+
 echo ""
 echo "RAW categories:"
 echo ""
-cat mc_2011_aug_raw.blocks | grep -v '/CMSSW_' | grep -vi test | grep -vi preprod | grep -v Backfill | grep -vi jobrobot | grep -vi sam | grep -v GEN-SIM-RAW | awk -F\/ '{print $3}' | sort -u
+cat mc_2011_sep_raw.blocks | grep -v '/CMSSW_' | grep -vi test | grep -vi preprod | grep -v Backfill | grep -vi jobrobot | grep -vi sam | grep -v GEN-SIM-RAW | awk -F\/ '{print $3}' | sort -u
 echo ""
 echo "RECO categories:"
 echo ""
-cat mc_2011_aug_reco.blocks | grep -v '/CMSSW_' | grep -vi test | grep -vi preprod | grep -v Backfill | grep -vi jobrobot | grep -vi sam | grep -v X0M | grep -v HCal | grep -v Flat | grep -v IDEAL  | grep -v NoPU | awk -F\/ '{print $3}' | sort -u
+cat mc_2011_sep_reco.blocks | grep -v '/CMSSW_' | grep -vi test | grep -vi preprod | grep -v Backfill | grep -vi jobrobot | grep -vi sam | grep -v X0M | grep -v Fall11 | grep -v CSCFail | grep -v STARTHI42 | grep -v PUpythia8 | grep -v FastSim | grep -v HCal | grep -v Flat | grep -v IDEAL  | grep -v NoPU | awk -F\/ '{print $3}' | sort -u
 echo ""
 echo "AOD categories:"
 echo ""
-cat mc_2011_aug_aod.blocks | grep -v '/CMSSW_' | grep -vi test | grep -vi preprod | grep -v Backfill | grep -vi jobrobot | grep -vi sam | grep -v X0M | grep -v HCal | grep -v Flat | grep -v IDEAL  | grep -v NoPU | grep -v Spring11 | awk -F\/ '{print $3}' | sort -u
+cat mc_2011_sep_aod.blocks | grep -v '/CMSSW_' | grep -vi test | grep -vi preprod | grep -v Backfill | grep -vi jobrobot | grep -vi sam | grep -v X0M | grep -v Fall11 | grep -v CSCFail | grep -v STARTHI42 | grep -v PUpythia8 | grep -v FastSim | grep -v HCal | grep -v Flat | grep -v IDEAL  | grep -v NoPU | grep -v Spring11 | awk -F\/ '{print $3}' | sort -u
 
-cat mc_2011_aug_raw.blocks | grep -v '/CMSSW_' | grep -vi test | grep -vi preprod | grep -v Backfill | grep -vi jobrobot | grep -vi sam | grep -v GEN-SIM-RAW | awk '{SUM1+=$2} {SUM2+=$3} END {print "August 2011 GEN-SIM-RAW: Size [TiB]: "SUM1/1024/1024/1024/1024" Events: "SUM2}'
-cat mc_2011_aug_reco.blocks | grep -v '/CMSSW_' | grep -vi test | grep -vi preprod | grep -v Backfill | grep -vi jobrobot | grep -vi sam | grep -v X0M | grep -v HCal | grep -v Flat | grep -v IDEAL   | grep -v NoPU | awk '{SUM1+=$2} {SUM2+=$3} END {print "August 2011 GEN-SIM-RECO: Size [TiB]: "SUM1/1024/1024/1024/1024" Events: "SUM2}'
-cat mc_2011_aug_aod.blocks | grep -v '/CMSSW_' | grep -vi test | grep -vi preprod | grep -v Backfill | grep -vi jobrobot | grep -vi sam | grep -v X0M | grep -v HCal | grep -v Flat | grep -v IDEAL | grep -v NoPU | grep -v Spring11 | awk '{SUM1+=$2} {SUM2+=$3} END {print "August 2011 AODSIM: Size [TiB]: "SUM1/1024/1024/1024/1024" Events: "SUM2}'
+echo ""
+cat mc_2011_sep_raw.blocks | grep -v '/CMSSW_' | grep -vi test | grep -vi preprod | grep -v Backfill | grep -vi jobrobot | grep -vi sam | grep -v GEN-SIM-RAW | awk '{SUM1+=$2} {SUM2+=$3} END {print "September 2011 GEN-SIM-RAW: Size [TiB]: "SUM1/1024/1024/1024/1024" Events: "SUM2}'
+cat mc_2011_sep_reco.blocks | grep -v '/CMSSW_' | grep -vi test | grep -vi preprod | grep -v Backfill | grep -vi jobrobot | grep -vi sam | grep -v X0M | grep -v Fall11 | grep -v CSCFail | grep -v STARTHI42 | grep -v PUpythia8 | grep -v FastSim | grep -v HCal | grep -v Flat | grep -v IDEAL   | grep -v NoPU | awk '{SUM1+=$2} {SUM2+=$3} END {print "September 2011 GEN-SIM-RECO: Size [TiB]: "SUM1/1024/1024/1024/1024" Events: "SUM2}'
+cat mc_2011_sep_aod.blocks | grep -v '/CMSSW_' | grep -vi test | grep -vi preprod | grep -v Backfill | grep -vi jobrobot | grep -vi sam | grep -v X0M | grep -v Fall11 | grep -v CSCFail | grep -v STARTHI42 | grep -v PUpythia8 | grep -v FastSim | grep -v HCal | grep -v Flat | grep -v IDEAL | grep -v NoPU | grep -v Spring11 | awk '{SUM1+=$2} {SUM2+=$3} END {print "September 2011 AODSIM: Size [TiB]: "SUM1/1024/1024/1024/1024" Events: "SUM2}'
