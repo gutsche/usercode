f783b81ab4ffeee5530fccd0d33a08ad14c1cf97
gutsche <>
2012-03-28 14:51:13 +0000

    added DR51X and DR52X

--- GutSoftConfigurations/Scripts/DBS/mcprod.py
+++ GutSoftConfigurations/Scripts/DBS/mcprod.py
@@ -3,20 +3,29 @@
 import sys,os,subprocess,shlex,locale
 locale.setlocale(locale.LC_ALL, 'en_US')
 
-result = {}
+def queryDBS(title,dataset_query):
+    result = {}
 
-commandline = "dbs search --query=\"find dataset.status,sum(block.numevents) where dataset = *Summer12*GEN-SIM\" --noheader --production"
-localargs = shlex.split(commandline)
-output = subprocess.Popen(localargs, shell=False, stdout=subprocess.PIPE)
-lines = output.communicate()[0].split('\n')
-for line in lines:
-    if line == '': continue
-    array = line.split()
-    result[array[0]] = int(array[1])
+    commandline = "dbs search --query=\"find dataset.status,sum(block.numevents) where dataset = "+dataset_query+"\" --noheader --production"
+    localargs = shlex.split(commandline)
+    output = subprocess.Popen(localargs, shell=False, stdout=subprocess.PIPE)
+    lines = output.communicate()[0].split('\n')
+    for line in lines:
+        if line == '': continue
+        array = line.split()
+        result[array[0]] = int(array[1])
     
-print ""
-print "%s: %s" % ("PRODUCTION".ljust(12),locale.format("%d", result['PRODUCTION'], grouping=True).rjust(20))
-print "%s: %s" % ("VALID".ljust(12),locale.format("%d", result['VALID'], grouping=True).rjust(20))
-print "----------------------------------"
-print "%s: %s" % ("TOTAL".ljust(12),locale.format("%d", result['VALID']+result['PRODUCTION'], grouping=True).rjust(20))
-print ""
+    print ""
+    print title+ ":"
+    print "----------------------------------"
+    print ""
+    print "%s: %s" % ("PRODUCTION".ljust(12),locale.format("%d", result['PRODUCTION'], grouping=True).rjust(20))
+    print "%s: %s" % ("VALID".ljust(12),locale.format("%d", result['VALID'], grouping=True).rjust(20))
+    print "----------------------------------"
+    print "%s: %s" % ("TOTAL".ljust(12),locale.format("%d", result['VALID']+result['PRODUCTION'], grouping=True).rjust(20))
+    print ""
+
+
+queryDBS("Summer12 GEN-SIM","*Summer12*GEN-SIM")
+queryDBS("Summer12 DR51X AODSIM","*Summer12-PU_S7_START50*AODSIM")
+queryDBS("Summer12 DR52X AODSIM","*Summer12-PU_S7_START52*AODSIM")
