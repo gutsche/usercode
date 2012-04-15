dd4d4d95bc6b0248728f12b185ffb7ee89c1884f
gutsche <>
2012-04-15 22:46:57 +0000

    add support for all different processings in Summer12 AODSIM

--- GutSoftConfigurations/Scripts/DBS/mcprod.py
+++ GutSoftConfigurations/Scripts/DBS/mcprod.py
@@ -3,6 +3,20 @@
 import sys,os,subprocess,shlex,locale
 locale.setlocale(locale.LC_ALL, 'en_US')
 
+def queryForDifferentCampaigns(era,tier):
+    campaigns = []
+    commandline = "dbs search --query=\"find dataset where dataset = *"+era+"*"+tier+"\" --noheader --production"
+    localargs = shlex.split(commandline)
+    output = subprocess.Popen(localargs, shell=False, stdout=subprocess.PIPE)
+    lines = output.communicate()[0].split('\n')
+    for line in lines:
+        if line == '': continue
+        array = line.split('/')
+        array2 = array[2].split('-')
+        if array2[1] not in campaigns: campaigns.append(array2[1])
+    return campaigns
+    
+
 def queryDBS(title,dataset_query):
     result = {}
 
@@ -16,7 +30,7 @@ def queryDBS(title,dataset_query):
         result[array[0]] = int(array[1])
     
     print ""
-    print title+ ":"
+    print title + " (DBS query string: " + dataset_query + "):"
     print "----------------------------------"
     print ""
     total = 0
@@ -30,7 +44,14 @@ def queryDBS(title,dataset_query):
     print "%s: %s" % ("TOTAL".ljust(12),locale.format("%d", total, grouping=True).rjust(20))
     print ""
 
-
+campaigns = queryForDifferentCampaigns("Summer12","AODSIM")
 queryDBS("Summer12 GEN-SIM","*Summer12*GEN-SIM")
 queryDBS("Summer12 DR51X AODSIM","*Summer12-PU_S7_START50*AODSIM")
+queryDBS("Summer12 DR51X NOPU AODSIM","*Summer12-NoPileUp_START50*AODSIM")
 queryDBS("Summer12 DR52X AODSIM","*Summer12-PU_S7_START52*AODSIM")
+for campaign in campaigns:
+    if campaign == "PU_S7_START50_V15": continue
+    if campaign == "PU_S7_START52_V5": continue
+    if campaign == 'NoPileUp_START50_V15' : continue
+    dataset_query = '*Summer12-' + campaign + '*AODSIM'
+    queryDBS("Summer12 AODSIM",dataset_query)
