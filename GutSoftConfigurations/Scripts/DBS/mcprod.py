a5173feffab2866ec8ed14258dfe6d20588f5135
gutsche <>
2012-09-13 18:31:59 +0000

    latest changed

--- GutSoftConfigurations/Scripts/DBS/mcprod.py
+++ GutSoftConfigurations/Scripts/DBS/mcprod.py
@@ -21,6 +21,7 @@ def queryDBS(title,dataset_query):
     result = {}
 
     commandline = "dbs search --query=\"find dataset.status,sum(block.numevents) where dataset = "+dataset_query+"\" --noheader --production"
+    #print commandline
     localargs = shlex.split(commandline)
     output = subprocess.Popen(localargs, shell=False, stdout=subprocess.PIPE)
     lines = output.communicate()[0].split('\n')
@@ -42,6 +43,9 @@ def queryDBS(title,dataset_query):
     if 'VALID' in result.keys():
         print "%s: %s" % ("VALID".ljust(12),locale.format("%d", result['VALID'], grouping=True).rjust(20))
         total += result['VALID']
+#    if 'INVALID' in result.keys():
+#        print "%s: %s" % ("INVALID".ljust(12),locale.format("%d", result['INVALID'], grouping=True).rjust(20))
+#        total += result['INVALID']    
     print "----------------------------------"
     print "%s: %s" % ("TOTAL".ljust(12),locale.format("%d", total, grouping=True).rjust(20))
     print ""
@@ -50,7 +54,7 @@ def queryDBS(title,dataset_query):
 queryDBS("Summer12 GEN-SIM","*Summer12*GEN-SIM")
 # queryDBS("Summer12 DR51X AODSIM","*Summer12-PU_S7_START50*AODSIM")
 # queryDBS("Summer12 DR51X NOPU AODSIM","*Summer12-NoPileUp_START50*AODSIM")
-queryDBS("Summer12 DR52X AODSIM","*Summer12-PU_S7_START52_V*AODSIM")
+# queryDBS("Summer12 DR52X AODSIM","*Summer12-PU_S7_START52_V*AODSIM")
 # queryDBS("Summer12 DR52X LowPU AODSIM","*Summer12-PU_S8_START52_V*AODSIM")
 # queryDBS("Summer12 DR52X TSG GEN-SIM-RECO","*Summer12-PU_S9_START52_V*GEN-SIM-RECO")
 # for campaign in campaigns:
@@ -60,6 +64,20 @@ queryDBS("Summer12 DR52X AODSIM","*Summer12-PU_S7_START52_V*AODSIM")
     # dataset_query = '*Summer12-' + campaign + '*AODSIM'
     # queryDBS("Summer12 AODSIM",dataset_query)
     # print campaign
-queryDBS("Summer12 DR53X AODSIM","*Summer12_DR53X*PU_S7*AODSIM")
+#queryDBS("Summer12 DR53X OLD AODSIM","*Summer12_DR53X*PU_S7*AODSIM")
 queryDBS("Summer12 DR53X AODSIM","*Summer12_DR53X*PU_S10*AODSIM")
-queryDBS("Upgrade MC AODSIM","*428_R2*AODSIM")
+#queryDBS("Upgrade MC GEN-SIM-DIGI-RECO","*428_R2*GEN-SIM-DIGI-RECO")
+#queryDBS("Upgrade MC StdGeom2 GEN-SIM-DIGI-RECO","*UpgradeStdGeom2_DR428_R2*GEN-SIM-DIGI-RECO")
+#queryDBS("Upgrade MC UpgradePhase1 GEN-SIM-DIGI-RECO","*UpgradePhase1_DR428_R2*GEN-SIM-DIGI-RECO")
+#queryDBS("Upgrade MC UpgradeHCAL_PixelPhase1 GEN-SIM-DIGI-RECO","*UpgradeHCAL_PixelPhase1_DR428_R2*GEN-SIM-DIGI-RECO")
+# queryDBS("Summer12 FastSim AODSIM","*Summer12*FSIM*AODSIM")
+# queryDBS("Run2010B Apr21","*Run2010B*Apr21*AOD")
+# queryDBS("Run2011A 08Nov2011","*Run2011A*08Nov2011*AOD")
+# queryDBS("Run2011B 19Nov2011","*Run2011B*19Nov2011*AOD")
+# queryDBS("Run2012A 13Jul2012","*Run2012A*13Jul2012*AOD")
+# queryDBS("Run2012B 13Jul2012","*Run2012B*13Jul2012*AOD")
+# queryDBS("Run2012B Parked","*Parked/Run2012B*RAW")
+# queryDBS("Run2012B Parked","/VBF1Parked/Run2012B*RAW")
+# queryDBS("Summer11 GEN-SIM","*Summer11*GEN-SIM")
+#queryDBS("Summer12 GEN-SIM","*Summer12*GEN-SIM")
+
