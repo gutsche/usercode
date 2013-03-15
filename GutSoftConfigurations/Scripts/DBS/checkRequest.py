630cf8dfbb72497c55d11222ff084b6be776bf48
gutsche <>
2013-03-15 23:14:37 +0000

    added directory check at FNAL

--- GutSoftConfigurations/Scripts/DBS/checkRequest.py
+++ GutSoftConfigurations/Scripts/DBS/checkRequest.py
@@ -2,6 +2,13 @@
 
 import sys,getopt,urllib2,json,os,datetime,subprocess,shlex
 
+def checkDirExists(lfn):
+    path = '/pnfs/cms/WAX/11' + lfn
+    result = os.path.exists(path)
+    if result == True: result = os.path.isdir(path)
+    return result
+
+
 requestID = None
 allSites = 1
 try:
@@ -224,19 +231,19 @@ if len(datasets) > 0 :
                 tmplfn = '/'+'/'.join(parts[1:6])
                 if tmplfn.count('/store/data/') > 0 or tmplfn.count('/store/hidata/') > 0 :
                     if tier in datatiers_cat1:
-                        if tmplfn_p not in lfn_cat1: lfn_cat1.append(tmplfn_p)
+                        if tmplfn_p not in lfn_cat1 and checkDirExists(tmplfn_p) == False : lfn_cat1.append(tmplfn_p)
                     else:
                         if tmplfn_p.count('Prompt') > 0:
-                            if tmplfn_p not in lfn_cat2: lfn_cat2.append(tmplfn_p)
+                            if tmplfn_p not in lfn_cat2 and checkDirExists(tmplfn_p) == False : lfn_cat2.append(tmplfn_p)
                         elif tmplfn_p.count('ALCARECO') > 0:
-                            if tmplfn_p not in lfn_cat2: lfn_cat2.append(tmplfn_p)
+                            if tmplfn_p not in lfn_cat2 and checkDirExists(tmplfn) == False : lfn_cat2.append(tmplfn_p)
                         else :
-                            if tmplfn not in lfn_cat3: lfn_cat3.append(tmplfn)
+                            if tmplfn not in lfn_cat3 and checkDirExists(tmplfn) == False : lfn_cat3.append(tmplfn)
                 else:
                     if tier in datatiers_cat4:
-                        if tmplfn not in lfn_cat4: lfn_cat4.append(tmplfn)
+                        if tmplfn not in lfn_cat4 and checkDirExists(tmplfn) == False : lfn_cat4.append(tmplfn)
                     else:
-                        if tmplfn not in lfn_cat5: lfn_cat5.append(tmplfn)
+                        if tmplfn not in lfn_cat5 and checkDirExists(tmplfn) == False : lfn_cat5.append(tmplfn)
 
 if len(blocks) > 0 :
     for block in blocks:
@@ -255,19 +262,19 @@ if len(blocks) > 0 :
             tmplfn = '/'+'/'.join(parts[1:6])
             if tmplfn.count('/store/data/') > 0 or tmplfn.count('/store/hidata/') > 0 :
                 if tier in datatiers_cat1:
-                    if tmplfn_p not in lfn_cat1: lfn_cat1.append(tmplfn_p)
+                    if tmplfn_p not in lfn_cat1 and checkDirExists(tmplfn_p) == False : lfn_cat1.append(tmplfn_p)
                 else:
                     if tmplfn_p.count('Prompt') > 0:
-                        if tmplfn_p not in lfn_cat2: lfn_cat2.append(tmplfn_p)
+                        if tmplfn_p not in lfn_cat2 and checkDirExists(tmplfn_p) == False : lfn_cat2.append(tmplfn_p)
                     elif tmplfn_p.count('ALCARECO') > 0:
-                        if tmplfn_p not in lfn_cat2: lfn_cat2.append(tmplfn_p)
+                        if tmplfn_p not in lfn_cat2 and checkDirExists(tmplfn) == False : lfn_cat2.append(tmplfn_p)
                     else :
-                        if tmplfn not in lfn_cat3: lfn_cat3.append(tmplfn)
+                        if tmplfn not in lfn_cat3 and checkDirExists(tmplfn) == False : lfn_cat3.append(tmplfn)
             else:
                 if tier in datatiers_cat4:
-                    if tmplfn not in lfn_cat4: lfn_cat4.append(tmplfn)
+                    if tmplfn not in lfn_cat4 and checkDirExists(tmplfn) == False : lfn_cat4.append(tmplfn)
                 else:
-                    if tmplfn not in lfn_cat5: lfn_cat5.append(tmplfn)
+                    if tmplfn not in lfn_cat5 and checkDirExists(tmplfn) == False : lfn_cat5.append(tmplfn)
 
 
 lfn = []
@@ -276,6 +283,12 @@ lfn.extend(lfn_cat2)
 lfn.extend(lfn_cat3)
 lfn.extend(lfn_cat4)
 lfn.extend(lfn_cat5)
+
+if len(lfn) <= 0:
+    print ''
+    print 'No tape families have to be created, all good to go!'
+    print ''
+    sys.exit(0)
 
 eras = []
 for item in lfn:
